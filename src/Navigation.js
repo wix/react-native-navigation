d1f36702a3fc9425ad2e6bb581bd7463a2746b7a
Guns <guns2410@users.noreply.github.com>
2017-01-05 22:58:09 +0530

    Support for ApolloStack

--- src/Navigation.js
+++ src/Navigation.js
@@ -13,9 +13,9 @@ function registerScreen(screenID, generator) {
   AppRegistry.registerComponent(screenID, generator);
 }
 
-function registerComponent(screenID, generator, store = undefined, Provider = undefined) {
+function registerComponent(screenID, generator, store = undefined, Provider = undefined, client = undefined) {
   if (store && Provider) {
-    return _registerComponentRedux(screenID, generator, store, Provider);
+    return _registerComponentRedux(screenID, generator, store, Provider, client);
   } else {
     return _registerComponentNoRedux(screenID, generator);
   }
@@ -52,7 +52,7 @@ function _registerComponentNoRedux(screenID, generator) {
   return generatorWrapper;
 }
 
-function _registerComponentRedux(screenID, generator, store, Provider) {
+function _registerComponentRedux(screenID, generator, store, Provider, client) {
   const generatorWrapper = function() {
     const InternalComponent = generator();
     return class extends Screen {
@@ -74,7 +74,7 @@ function _registerComponentRedux(screenID, generator, store, Provider) {
 
       render() {
         return (
-          <Provider store={store}>
+          <Provider store={store} client={client}>
             <InternalComponent testID={screenID} navigator={this.navigator} {...this.state.internalProps} />
           </Provider>
         );
