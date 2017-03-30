7fa43fa7759276e3781833e94bc67224ee6032ba
Aiden Montgomery <aiden@constructivecoding.com>
2017-03-30 21:34:36 +0100

    Added an additional navigation event for the press of an already selected tab

--- android/app/src/main/java/com/reactnativenavigation/layouts/BottomTabsLayout.java
+++ android/app/src/main/java/com/reactnativenavigation/layouts/BottomTabsLayout.java
@@ -317,6 +317,7 @@ public class BottomTabsLayout extends BaseLayout implements AHBottomNavigation.O
     @Override
     public boolean onTabSelected(int position, boolean wasSelected) {
         if (wasSelected) {
+            sendTabPressedEventToJs();
             return false;
         }
         
@@ -331,6 +332,12 @@ public class BottomTabsLayout extends BaseLayout implements AHBottomNavigation.O
         WritableMap data = Arguments.createMap();
         String navigatorEventId = getCurrentScreenStack().peek().getNavigatorEventId();
         NavigationApplication.instance.getEventEmitter().sendNavigatorEvent("bottomTabSelected", navigatorEventId, data);
+    }
+
+    private void sendTabPressedEventToJs() {
+        WritableMap data = Arguments.createMap();
+        String navigatorEventId = getCurrentScreenStack().peek().getNavigatorEventId();
+        NavigationApplication.instance.getEventEmitter().sendNavigatorEvent("bottomTabPressed", navigatorEventId, data);
     }
 
     private void showNewStack(int position) {
