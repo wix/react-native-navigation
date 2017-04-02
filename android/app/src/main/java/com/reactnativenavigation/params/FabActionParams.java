d24c77e6f3d1995e193af1ce8ad61c96a1db0512
Justo Sierra <justsierra26@gmail.com>
2017-04-02 13:43:34 -0400

    add title fab action param

--- android/app/src/main/java/com/reactnativenavigation/params/FabActionParams.java
+++ android/app/src/main/java/com/reactnativenavigation/params/FabActionParams.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.Drawable;
 public class FabActionParams {
     public String id;
     public String navigatorEventId;
+    public String title;
     public Drawable icon;
     public StyleParams.Color backgroundColor;
 }
