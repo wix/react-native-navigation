cd7e986a89d66e27f1256430a6d593be97648843
charlesluo2014 <lishu.luo@foxmail.com>
2017-04-09 15:41:47 +0800

    add boot

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import {Navigation,NavigationToolBarIOS} from './deprecated/indexDeprecated';
 import Router from './Router';
+import {Navbar,Tabbar,Screen,AuthScreen,Group,Root,Drawer,Login,Logout,Api,ApiRegistry,Bootstrap} from './boot';
+import HttpRequest from './utils/request';
 
-export  * from './boot';
-export HttpRequest from './utils/request';
 
 module.exports = {
     Router,
+    Navbar,
+    Tabbar,
+    Screen,
+    AuthScreen,
+    Group,
+    Root,
+    Drawer,
+    Login,
+    Logout,
+    Api,
+    ApiRegistry,
+    Bootstrap,
+    HttpRequest,
     Navigation,
     NavigationToolBarIOS
 }
