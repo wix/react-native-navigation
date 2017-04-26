d4cb89159248b5a84abd129cfcc276b7cfa32688
Elliot Hesp <elliot.hesp@gmail.com>
2017-04-26 09:26:17 +0100

    Add app icon

--- example/src/app.js
+++ example/src/app.js
@@ -57,21 +57,3 @@ Navigation.startTabBasedApp({
         }
     }
 });
-//Navigation.startSingleScreenApp({
-//  screen: {
-//    screen: 'example.FirstTabScreen',
-//    title: 'Navigation',
-//    navigatorStyle: {
-//      navBarBackgroundColor: '#4dbce9',
-//      navBarTextColor: '#ffff00',
-//      navBarSubtitleTextColor: '#ff0000',
-//      navBarButtonColor: '#ffffff',
-//      statusBarTextColorScheme: 'light'
-//    }
-//  },
-//  drawer: {
-//    left: {
-//      screen: 'example.SideMenu'
-//    }
-//  }
-//});
