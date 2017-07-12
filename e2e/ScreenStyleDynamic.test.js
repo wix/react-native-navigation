faaae1f079e6e7ec46846d547a7d569282d92f73
Daniel Zlotin <zlotindaniel@gmail.com>
2017-07-12 15:42:51 +0300

    Update ScreenStyleDynamic.test.js

--- e2e/ScreenStyleDynamic.test.js
+++ e2e/ScreenStyleDynamic.test.js
@@ -6,7 +6,7 @@ describe('screen style - dynamic', () => {
     await device.relaunchApp();
   });
 
-  it.only('change title on container component', async () => {
+  it('change title on container component', async () => {
     await elementByLabel('Push Options Screen').tap();
     await expect(element(by.label('Static Title').and(by.type('UILabel')))).toBeVisible();
     await elementByLabel('Dynamic Options').tap();
