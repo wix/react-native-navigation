9c435057dbed8927f807a2e9ccb5761d24ca2486
Guy Carmeli <guyc@wix.com>
2017-07-17 09:32:20 +0300

    Think I better assign a value to emitter

--- src/ScreenVisibilityListener.js
+++ src/ScreenVisibilityListener.js
@@ -5,16 +5,16 @@ import {
 } from 'react-native';
 export default class ScreenVisibilityListener {
   constructor(listeners) {
-    const emitter = Platform.OS === 'android' ? DeviceEventEmitter : NativeAppEventEmitter;
+    this.emitter = Platform.OS === 'android' ? DeviceEventEmitter : NativeAppEventEmitter;
     this.listeners = listeners;
   }
 
   register() {
     const {willAppear, didAppear, willDisappear, didDisappear} = this.listeners;
-    willAppear && emitter.addListener('willAppear', willAppear);
-    didAppear && emitter.addListener('didAppear', didAppear);
-    willDisappear && emitter.addListener('willDisappear', willDisappear);
-    didDisappear && emitter.addListener('didDisappear', didDisappear);
+    willAppear && this.emitter.addListener('willAppear', willAppear);
+    didAppear && this.emitter.addListener('didAppear', didAppear);
+    willDisappear && this.emitter.addListener('willDisappear', willDisappear);
+    didDisappear && this.emitter.addListener('didDisappear', didDisappear);
   }
 
 }
