b89b16dbf4fa29b0e7d855615609857d11cd74a0
PC Futures <jake@pcfutures.co.uk>
2017-08-06 18:13:36 +0100

    Make Navigator public for Android too

--- src/deprecated/indexDeprecated.android.js
+++ src/deprecated/indexDeprecated.android.js
@@ -2,6 +2,7 @@ import Navigation from './../Navigation';
 import SharedElementTransition from './../views/sharedElementTransition';
 import NativeEventsReceiver from './../NativeEventsReceiver';
 import ScreenVisibilityListener from './../ScreenVisibilityListener';
+import { Navigator } from './../Screen';
 
 module.exports = {
   Navigation,
