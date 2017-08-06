373af5eef10f37027f486dafbdce499f2cc05f3c
PC Futures <jake@pcfutures.co.uk>
2017-08-06 18:12:08 +0100

    Make Screen.js export both Screen and Navigator

--- src/Screen.js
+++ src/Screen.js
@@ -175,7 +175,7 @@ class Navigator {
   }
 }
 
-export default class Screen extends Component {
+class Screen extends Component {
   static navigatorStyle = {};
   static navigatorButtons = {};
 
@@ -193,3 +193,5 @@ export default class Screen extends Component {
     }
   }
 }
+
+export { Screen, Navigator };
