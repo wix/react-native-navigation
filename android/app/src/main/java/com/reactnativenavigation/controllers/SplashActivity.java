000ffcba67d801eab37bab45290a8d34df1da5a2
Brendon Sled <brendon.sled@skipthedishes.ca>
2017-08-10 09:31:44 -0600

    Fixed app restarting if launched from the home screen

--- android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
@@ -14,10 +14,15 @@ public abstract class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, NavigationActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        intent.putExtra("SplashLayout", getSplashLayout());
-        startActivity(intent);
+        if (isTaskRoot()) {
+            Intent intent = new Intent(this, NavigationActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            intent.putExtra("SplashLayout", getSplashLayout());
+            startActivity(intent);
+        } else {
+            finish();
+            overridePendingTransition(0,0);
+        }
     }
 
     @Override
