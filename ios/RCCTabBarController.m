0a59046eb75fdbe82930135f0b7753df5f3120a4
Egor Khmelev <hmelyoff@gmail.com>
2017-08-11 11:18:57 +0300

    Introduce support of overlayView for TabBar based apps

--- ios/RCCTabBarController.m
+++ ios/RCCTabBarController.m
@@ -14,6 +14,12 @@
 
 @end
 
+@interface RCCTabBarController()
+
+@property (strong, nonatomic) RCTRootView *overlayView;
+
+@end
+
 @implementation RCCTabBarController
 
 
@@ -84,6 +90,7 @@
   UIColor *selectedButtonColor = nil;
   UIColor *labelColor = nil;
   UIColor *selectedLabelColor = nil;
+  NSDictionary *passProps = props[@"passProps"];
   NSDictionary *tabsStyle = props[@"style"];
   if (tabsStyle)
   {
@@ -131,6 +138,19 @@
     {
       self.tabBar.clipsToBounds = [tabBarHideShadow boolValue] ? YES : NO;
     }
+  }
+  
+  NSString *overlayView = [props valueForKeyPath:@"overlay.view"];
+  if (overlayView) {
+    self.overlayView = [[RCTRootView alloc] initWithBridge:bridge moduleName:overlayView initialProperties:passPropsOverlay];
+    // Pass navigation props
+    NSMutableDictionary *mutablePassPropsOverlay = [passProps mutableCopy];
+    NSDictionary *overlayProps = [props valueForKeyPath:@"overlay.passProps"];
+    if (overlayProps) {
+      [mutablePassPropsOverlay addEntriesFromDictionary:overlayProps];
+    }
+    
+    NSDictionary *passPropsOverlay = [NSDictionary dictionaryWithDictionary:mutablePassPropsOverlay];
   }
   
   NSMutableArray *viewControllers = [NSMutableArray array];
@@ -229,6 +249,33 @@
   [self setRotation:props];
   
   return self;
+}
+
+- (void)setOverlayView:(RCTRootView *)overlayView {
+  RCTRootView *previousOverlayView = _overlayView;
+  
+  if (previousOverlayView) {
+    [previousOverlayView removeFromSuperview];
+  }
+  
+  _overlayView = overlayView;
+  
+  if (!_overlayView) {
+    return;
+  }
+  
+  _overlayView.passThroughTouches = YES;
+  _overlayView.backgroundColor = [UIColor clearColor];
+  _overlayView.frame = self.view.bounds;
+  [self.view addSubview:_overlayView];
+}
+
+- (void)viewDidLayoutSubviews {
+  [super viewDidLayoutSubviews];
+  
+  if (_overlayView) {
+    [self.view bringSubviewToFront:_overlayView];
+  }
 }
 
 - (void)performAction:(NSString*)performAction actionParams:(NSDictionary*)actionParams bridge:(RCTBridge *)bridge completion:(void (^)(void))completion
--- src/deprecated/platformSpecificDeprecated.ios.js
+++ src/deprecated/platformSpecificDeprecated.ios.js
@@ -88,6 +88,7 @@ function startTabBasedApp(params) {
           style={params.tabsStyle}
           appStyle={params.appStyle}
           initialTabIndex={params.initialTabIndex}>
+          overlay={params.overlay}
           {
             params.tabs.map(function(tab, index) {
               return (
