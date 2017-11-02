679bad6209c1c0b2fa605e76da76553bba6326b7
Roman Kozlov <romanko@wix.com>
2017-11-02 15:52:51 +0200

    fix js tests

--- playground/src/containers/CustomDialog.js
+++ playground/src/containers/CustomDialog.js
@@ -6,42 +6,42 @@ const Navigation = require('react-native-navigation');
 
 class CustomDialog extends PureComponent {
 
-    render() {
-        return (
-            <View style={styles.root}>
-                <Text style={styles.h1}>Test view</Text>
-                <Button title="OK" onPress={this.onCLickOk} />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.root}>
+        <Text style={styles.h1}>Test view</Text>
+        <Button title="OK" onPress={this.onCLickOk} />
+      </View>
+    );
+  }
 
-    onCLickOk() {
-        Navigation.dismissOverlay();
-    }
+  onCLickOk() {
+    Navigation.dismissOverlay();
+  }
 }
 
 const styles = {
-    root: {
-        flexGrow: 1,
-        backgroundColor: 'green',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    h1: {
-        fontSize: 24,
-        textAlign: 'center',
-        margin: 10
-    },
-    h2: {
-        fontSize: 12,
-        textAlign: 'center',
-        margin: 10
-    },
-    footer: {
-        fontSize: 10,
-        color: '#888',
-        marginTop: 10
-    }
+  root: {
+    flexGrow: 1,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  h1: {
+    fontSize: 24,
+    textAlign: 'center',
+    margin: 10
+  },
+  h2: {
+    fontSize: 12,
+    textAlign: 'center',
+    margin: 10
+  },
+  footer: {
+    fontSize: 10,
+    color: '#888',
+    marginTop: 10
+  }
 };
 
 module.exports = CustomDialog;
