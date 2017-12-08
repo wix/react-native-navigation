3b65b7c9b3c3628a8159f79ff9091bf04dee3d17
Ioannis Kokkinidis <sudoplz@gmail.com>
2017-12-08 17:59:41 +0200

    Now allowing the custom nav bar to take up the whole space on iOS
    - Also the custom nav bar reacts well to orientation changes
    - Fixes: https://github.com/wix/react-native-navigation/issues/1893

--- ios/RCCCustomTitleView.m
+++ ios/RCCCustomTitleView.m
@@ -11,13 +11,16 @@
 @interface RCCCustomTitleView ()
 @property (nonatomic, strong) UIView *subView;
 @property (nonatomic, strong) NSString *subViewAlign;
+@property BOOL correctedTheNavBarWidth;
+@property CGRect correctFrame;
 @end
 
 @implementation RCCCustomTitleView
 
 
 -(instancetype)initWithFrame:(CGRect)frame subView:(UIView*)subView alignment:(NSString*)alignment {
-    self = [super initWithFrame:frame];
+    _correctFrame = frame;
+    self = [super initWithFrame:_correctFrame];
     
     if (self) {
         self.backgroundColor = [UIColor clearColor];
@@ -50,6 +53,44 @@
         reactViewFrame.origin.x = paddingLeftFromCenter - minPadding;
         self.subView.frame = reactViewFrame;
     }
+}
+
+- (void)setFrame:(CGRect) newFrame {
+    float correctNavBarWidth = _correctFrame.size.width;
+    float newNavBarWidth = newFrame.size.width;
+    BOOL frameNeedsToBeCorrected = newNavBarWidth < correctNavBarWidth;
+
+    if (_correctedTheNavBarWidth == NO) {
+        if (frameNeedsToBeCorrected) {
+            // first we need to find out the total horizontal margin
+            float navBarHorizontalMargin = correctNavBarWidth - newNavBarWidth;
+            
+            // then we need to place the nav bar half times the horizontal margin to the left
+            newFrame.origin.x -= navBarHorizontalMargin / 2;
+            
+            // and finally set the width to whatever we initially intended the width to be
+            newFrame.size.width = correctNavBarWidth;
+            
+            [super setFrame:newFrame];
+            _correctedTheNavBarWidth = YES;
+        } else { // if frame needs no correction
+            [super setFrame:newFrame];
+        }
+    }
+}
+
+
+- (void) viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    // whenever the orientation changes this runs
+    // and sets the nav bar item width to the new size width
+    CGRect newFrame = self.frame;
+
+    if (newFrame.size.width < size.width) {
+        newFrame.size.width = size.width;
+        _correctFrame = newFrame;
+    }
+    [super setFrame:newFrame];
+    [self setNeedsLayout];
 }
 
 @end
