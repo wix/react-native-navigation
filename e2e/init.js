a2ad4a16e9556ed6b16670c65213530ceea27d21
Guy Carmeli <guyc@wix.com>
2017-12-20 16:02:17 +0200

    Rebase fixes

--- e2e/init.js
+++ e2e/init.js
@@ -3,20 +3,7 @@ const detox = require('detox');
 const config = require('../package.json').detox;
 
 before(async () => {
-<<<<<<< HEAD
   await detox.init(config, { launchApp: false });
-=======
-  const detoxConfig = {
-    configurations: {
-      ios: {
-        binaryPath: process.env.detoxAppBuildPath,
-        type: 'ios.simulator',
-        name: 'iPhone 7'
-      }
-    }
-  };
-  await detox.init(detoxConfig, { launchApp: false });
->>>>>>> upstream merge
 });
 
 after(async () => {
