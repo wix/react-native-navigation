cf4cd826424d5f8b49de7d80f4ec9e781a2959cf
amorenovalls <amorenovalls@gmail.com>
2018-01-16 19:10:29 +0100

    Support to React Native 0.52.0

--- android/app/src/main/java/com/reactnativenavigation/react/JsDevReloadListenerReplacer.java
+++ android/app/src/main/java/com/reactnativenavigation/react/JsDevReloadListenerReplacer.java
@@ -1,8 +1,10 @@
 package com.reactnativenavigation.react;
 
+import android.app.Activity;
+
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.bridge.JavaJSExecutor;
-import com.facebook.react.devsupport.ReactInstanceDevCommandsHandler;
+import com.facebook.react.devsupport.ReactInstanceManagerDevHelper;
 import com.reactnativenavigation.utils.ReflectionUtils;
 
 class JsDevReloadListenerReplacer {
@@ -19,7 +21,7 @@ class JsDevReloadListenerReplacer {
     }
 
     void replace() {
-        ReactInstanceDevCommandsHandler originalHandler = getOriginalHandler();
+        ReactInstanceManagerDevHelper originalHandler = getOriginalHandler();
         DevCommandsHandlerProxy proxy = new DevCommandsHandlerProxy(originalHandler, listener);
         replaceInReactInstanceManager(proxy);
         replaceInDevSupportManager(proxy);
@@ -30,19 +32,19 @@ class JsDevReloadListenerReplacer {
         ReflectionUtils.setField(devSupportManager, "mReactInstanceCommandsHandler", proxy);
     }
 
-    private ReactInstanceDevCommandsHandler getOriginalHandler() {
-        return (ReactInstanceDevCommandsHandler) ReflectionUtils.getDeclaredField(reactInstanceManager, "mDevInterface");
+    private ReactInstanceManagerDevHelper getOriginalHandler() {
+        return (ReactInstanceManagerDevHelper) ReflectionUtils.getDeclaredField(reactInstanceManager, "mDevInterface");
     }
 
     private void replaceInReactInstanceManager(DevCommandsHandlerProxy proxy) {
         ReflectionUtils.setField(reactInstanceManager, "mDevInterface", proxy);
     }
 
-    private static class DevCommandsHandlerProxy implements ReactInstanceDevCommandsHandler {
-        private ReactInstanceDevCommandsHandler originalReactHandler;
+    private static class DevCommandsHandlerProxy implements ReactInstanceManagerDevHelper {
+        private ReactInstanceManagerDevHelper originalReactHandler;
         private final Listener listener;
 
-        DevCommandsHandlerProxy(ReactInstanceDevCommandsHandler originalReactHandler, Listener listener) {
+        DevCommandsHandlerProxy(ReactInstanceManagerDevHelper originalReactHandler, Listener listener) {
             this.originalReactHandler = originalReactHandler;
             this.listener = listener;
         }
@@ -63,5 +65,11 @@ class JsDevReloadListenerReplacer {
         public void toggleElementInspector() {
             originalReactHandler.toggleElementInspector();
         }
+
+        @Override
+        public Activity getCurrentActivity() {
+            return null;
+        }
+
     }
 }
