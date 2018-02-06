f0625d64e330761ca74beea73d382d1a5e76d7ea
Son, Joon <Joon.Son@nfl.com>
2018-02-06 11:50:41 -0800

    Prevent navigation from assertion crashing when restarting the app during the initialization phase

--- lib/ios/RNNEventEmitter.m
+++ lib/ios/RNNEventEmitter.m
@@ -52,6 +52,9 @@ static NSString* const onNavigationButtonPressed	= @"RNN.navigationButtonPressed
 # pragma mark private
 
 -(void)send:(NSString *)eventName body:(id)body {
+    if ([eventName isEqualToString:containerDidDisappear] && self.bridge == nil) {
+        return;
+    }
 	[self sendEventWithName:eventName body:body];
 }
 
