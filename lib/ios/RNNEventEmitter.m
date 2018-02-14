ab156a9a2b57ed97feb2115230e4dfc066950391
Son, Joon <Joon.Son@nfl.com>
2018-02-13 19:10:37 -0800

    fix typo

--- lib/ios/RNNEventEmitter.m
+++ lib/ios/RNNEventEmitter.m
@@ -57,7 +57,7 @@ static NSString* const navigationCommands	= @"RNN.navigationCommands";
 # pragma mark private
 
 -(void)send:(NSString *)eventName body:(id)body {
-    if ([eventName isEqualToString:containerDidDisappear] && self.bridge == nil) {
+    if ([eventName isEqualToString:componentDidDisappear] && self.bridge == nil) {
         return;
     }
 	[self sendEventWithName:eventName body:body];
