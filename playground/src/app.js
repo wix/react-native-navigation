d47d07e7341764a4fc8a481217b8c2645ec694b0
Roman Kozlov <romanko@wix.com>
2018-03-27 17:43:21 +0300

    anim

--- playground/src/app.js
+++ playground/src/app.js
@@ -24,7 +24,7 @@ function start() {
   registerScreens();
   Navigation.events().onAppLaunched(() => {
     Navigation.setDefaultOptions({
-      animations: {
+      _animations: {
         startApp: {
           y: {
             from: 1000,
