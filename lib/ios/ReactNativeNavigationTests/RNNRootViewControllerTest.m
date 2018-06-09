422f1a1bd4fd5136ad713db171a1c7df7a0ed3af
yogevbd <yogevbd@wix.com>
2018-06-09 23:42:40 +0300

    fix unit tests

--- lib/ios/ReactNativeNavigationTests/RNNRootViewControllerTest.m
+++ lib/ios/ReactNativeNavigationTests/RNNRootViewControllerTest.m
@@ -564,27 +564,6 @@
 	XCTAssertNil([self.uut.navigationController.navigationBar viewWithTag:BLUR_TOPBAR_TAG]);
 }
 
--(void)testTopBarLargeTitle_default {
-	__unused UINavigationController* nav = [[UINavigationController alloc] initWithRootViewController:self.uut];
-	[self.uut viewWillAppear:false];
-
-	XCTAssertEqual(self.uut.navigationItem.largeTitleDisplayMode,  UINavigationItemLargeTitleDisplayModeNever);
-}
--(void)testTopBarLargeTitle_true {
-	self.options.topBar.largeTitle = @(1);
-	__unused UINavigationController* nav = [[UINavigationController alloc] initWithRootViewController:self.uut];
-	[self.uut viewWillAppear:false];
-
-	XCTAssertEqual(self.uut.navigationItem.largeTitleDisplayMode, UINavigationItemLargeTitleDisplayModeAlways);
-}
--(void)testTopBarLargeTitle_false {
-	self.options.topBar.largeTitle  = @(0);
-	__unused UINavigationController* nav = [[UINavigationController alloc] initWithRootViewController:self.uut];
-	[self.uut viewWillAppear:false];
-
-	XCTAssertEqual(self.uut.navigationItem.largeTitleDisplayMode, UINavigationItemLargeTitleDisplayModeNever);
-}
-
 -(void)testTopBarBlur_false {
 	NSNumber* topBarBlurInput = @(0);
 	self.options.topBar.blur = topBarBlurInput;
