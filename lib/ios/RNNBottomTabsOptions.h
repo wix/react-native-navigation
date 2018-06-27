7ae0bee892ceecce6b828ca7d42c2308b6062334
yogevbd <yogevbd@wix.com>
2018-06-27 13:34:24 +0300

    fixed unit tests

--- lib/ios/RNNBottomTabsOptions.h
+++ lib/ios/RNNBottomTabsOptions.h
@@ -9,8 +9,12 @@
 @property (nonatomic, strong) NSNumber* drawBehind;
 @property (nonatomic, strong) NSString* currentTabId;
 
+@property (nonatomic, strong) NSNumber* tabColor;
+@property (nonatomic, strong) NSNumber* selectedTabColor;
 @property (nonatomic, strong) NSNumber* translucent;
 @property (nonatomic, strong) NSNumber* hideShadow;
 @property (nonatomic, strong) NSNumber* backgroundColor;
+@property (nonatomic, strong) NSString* fontFamily;
+@property (nonatomic, strong) NSNumber* fontSize;
 
 @end
