f2500be9f706721a3a697856996db0a2ab7d552a
Ujwal Setlur <ujwal.setlur@gmail.com>
2018-07-18 13:01:08 -0700

    Removed possibleOpenGesture MMOpenDrawerGestureModePanningCenterView possibleOpenGestureModesForGestureRecognizer. This allows swipeable componenets like swipeable lists to be used on iOS.

--- lib/ios/RNNSideMenu/MMDrawerController/MMDrawerController.m
+++ lib/ios/RNNSideMenu/MMDrawerController/MMDrawerController.m
@@ -1479,9 +1479,6 @@ static inline CGFloat originXForDrawerOriginAndTargetOriginOffset(CGFloat origin
         if([self isPointContainedWithinNavigationRect:point]){
             possibleOpenGestureModes |= MMOpenDrawerGestureModePanningNavigationBar;
         }
-        if([self isPointContainedWithinCenterViewContentRect:point]){
-            possibleOpenGestureModes |= MMOpenDrawerGestureModePanningCenterView;
-        }
         if([self isPointContainedWithinLeftBezelRect:point] &&
            self.leftDrawerViewController){
             possibleOpenGestureModes |= MMOpenDrawerGestureModeBezelPanningCenterView;
