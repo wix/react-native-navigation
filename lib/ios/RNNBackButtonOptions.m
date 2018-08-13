9e0cbd3ce6b2348d9722197b18f8fd09c7e1855e
Ujwal Setlur <ujwal.setlur@gmail.com>
2018-08-13 06:48:13 +0530

    Allow styling of default back button on iOS.

--- lib/ios/RNNBackButtonOptions.m
+++ lib/ios/RNNBackButtonOptions.m
@@ -11,11 +11,44 @@
 		
 		UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithImage:image style:UIBarButtonItemStylePlain target:nil action:nil];
 		viewController.navigationItem.backBarButtonItem = backItem;
-	} else if (self.title) {
-		UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithTitle:self.title
+	} else {
+		NSString *title;
+
+		if(self.title) {
+			title = self.title;
+		} else {
+			title = viewController.navigationItem.title;
+		}
+
+		UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithTitle:title
 																	 style:UIBarButtonItemStylePlain
 																	target:nil
 																	action:nil];
+
+		if (self.color) {
+			[backItem setTintColor:[RCTConvert UIColor:self.color]];
+		}
+
+		if(self.fontFamily || self.fontSize) {
+			NSNumber* fontSize = self.fontSize;
+			NSString* fontFamily = self.fontFamily;
+			NSMutableDictionary* textAttributes = [[NSMutableDictionary alloc] init];
+			UIFont *font = nil;
+
+			if (!fontSize) {
+				fontSize = [[NSNumber alloc] initWithInt: 18];
+			}
+
+			if (fontFamily) {
+				font = [UIFont fontWithName:fontFamily size:[fontSize floatValue]];
+			} else {
+				font = [UIFont systemFontOfSize:[fontSize floatValue]];
+			}
+			[textAttributes setObject:font forKey:NSFontAttributeName];
+
+			[backItem setTitleTextAttributes:textAttributes forState:UIControlStateNormal];
+			[backItem setTitleTextAttributes:textAttributes forState:UIControlStateHighlighted];
+		}
 		
 		viewController.navigationItem.backBarButtonItem = backItem;
 	}
