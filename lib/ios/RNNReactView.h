06896cecedc7ac6898f4d61bc1c0d80548e2398d
yogevbd <yogevbd@wix.com>
2018-09-04 12:10:10 +0300

    Refactored modals

---
+++ lib/ios/RNNReactView.h
@@ -0,0 +1,13 @@
+//
+//  RNNReactView.h
+//  ReactNativeNavigation
+//
+//  Created by Yogev Ben David on 04/09/2018.
+//  Copyright © 2018 Wix. All rights reserved.
+//
+
+#import "RCTRootView.h"
+
+@interface RNNReactView : RCTRootView
+
+@end
---
+++ lib/ios/RNNReactView.m
@@ -0,0 +1,21 @@
+//
+//  RNNReactView.m
+//  ReactNativeNavigation
+//
+//  Created by Yogev Ben David on 04/09/2018.
+//  Copyright © 2018 Wix. All rights reserved.
+//
+
+#import "RNNReactView.h"
+
+@implementation RNNReactView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
