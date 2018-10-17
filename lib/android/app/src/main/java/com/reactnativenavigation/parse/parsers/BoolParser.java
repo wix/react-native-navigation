658e281898e184fba44e28cbc91eb27883d78647
Evan Ricketts <evan.ricketts@bitmex.com>
2018-10-17 14:01:18 -0700

    chore(BoolParser): add nullability annotations

--- lib/android/app/src/main/java/com/reactnativenavigation/parse/parsers/BoolParser.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/parse/parsers/BoolParser.java
@@ -1,12 +1,14 @@
 package com.reactnativenavigation.parse.parsers;
 
+import android.support.annotation.NonNull;
+
 import com.reactnativenavigation.parse.params.Bool;
 import com.reactnativenavigation.parse.params.NullBool;
 
 import org.json.JSONObject;
 
 public class BoolParser {
-    public static Bool parse(JSONObject json, String bool) {
+    public static @NonNull Bool parse(@NonNull JSONObject json, @NonNull String bool) {
         return json.has(bool) ? new Bool(json.optBoolean(bool)) : new NullBool();
     }
 }
