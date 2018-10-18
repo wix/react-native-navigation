d1f0b429b60d226e4554616e1412b614169bcb41
yogevbd <yogevbd@wix.com>
2018-10-18 11:21:59 +0300

    Added tests for RNNTabBarController

--- lib/ios/ReactNativeNavigationTests/RNNTabBarControllerTest.m
+++ lib/ios/ReactNativeNavigationTests/RNNTabBarControllerTest.m
@@ -1,7 +1,8 @@
 #import <XCTest/XCTest.h>
 #import "RNNTabBarController.h"
 #import "RNNNavigationOptions.h"
-#import "RNNViewControllerPresenter.h"
+#import "RNNTabBarPresenter.h"
+#import <OCMock/OCMock.h>
 
 @interface RNNTabBarControllerTest : XCTestCase
 
@@ -28,6 +29,50 @@
 	self.uut = [[RNNTabBarController alloc] initWithLayoutInfo:nil childViewControllers:@[vc1, vc2] options:[[RNNNavigationOptions alloc] initWithDict:@{}] presenter:[[RNNViewControllerPresenter alloc] init]];
 	XCTAssertTrue(self.uut.viewControllers.count == 2);
 }
+
+- (void)testInitWithLayoutInfo_shouldInitializeDependencies {
+	RNNLayoutInfo* layoutInfo = [RNNLayoutInfo new];
+	RNNNavigationOptions* options = [[RNNNavigationOptions alloc] initWithDict:@{}];
+	RNNTabBarPresenter* presenter = [[RNNTabBarPresenter alloc] init];
+	NSArray* childViewControllers = @[[UIViewController new]];
+	
+	self.uut = [[RNNTabBarController alloc] initWithLayoutInfo:layoutInfo childViewControllers:childViewControllers options:options presenter:presenter];
+	XCTAssertTrue(self.uut.layoutInfo == layoutInfo);
+	XCTAssertTrue(self.uut.options == options);
+	XCTAssertTrue(self.uut.presenter == presenter);
+	XCTAssertTrue(self.uut.childViewControllers.count == childViewControllers.count);
+}
+
+- (void)testInitWithEventEmmiter_shouldInitializeDependencies {
+	RNNLayoutInfo* layoutInfo = [RNNLayoutInfo new];
+	RNNNavigationOptions* options = [[RNNNavigationOptions alloc] initWithDict:@{}];
+	RNNTabBarPresenter* presenter = [[RNNTabBarPresenter alloc] init];
+	RNNEventEmitter* eventEmmiter = [RNNEventEmitter new];
+
+	NSArray* childViewControllers = @[[UIViewController new]];
+	
+	self.uut = [[RNNTabBarController alloc] initWithLayoutInfo:layoutInfo childViewControllers:childViewControllers options:options presenter:presenter eventEmitter:eventEmmiter];
+	XCTAssertTrue(self.uut.layoutInfo == layoutInfo);
+	XCTAssertTrue(self.uut.options == options);
+	XCTAssertTrue(self.uut.presenter == presenter);
+	XCTAssertTrue(self.uut.childViewControllers.count == childViewControllers.count);
+	XCTAssertTrue(self.uut.eventEmitter == eventEmmiter);
+}
+
+- (void)testInitWithLayoutInfo_shouldSetDelegate {
+	self.uut = [[RNNTabBarController alloc] initWithLayoutInfo:nil childViewControllers:nil options:[[RNNNavigationOptions alloc] initWithDict:@{}] presenter:[[RNNViewControllerPresenter alloc] init]];
+	
+	XCTAssertTrue(self.uut.delegate == self.uut);
+}
+
+- (void)testWillMoveToParent_invokePresenterApplyOptionsOnWillMoveToParent {
+	id presenterMock = OCMClassMock([RNNTabBarPresenter class]);
+	self.uut = [[RNNTabBarController alloc] initWithLayoutInfo:nil childViewControllers:nil options:[[RNNNavigationOptions alloc] initWithDict:@{}] presenter:presenterMock];
+	[self.uut willMoveToParentViewController:[UIViewController new]];
+	
+	
+}
+
 
 
 @end
