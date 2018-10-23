7c3c22a3abe8b523642113c9cc3a909aafe34528
Guy Carmeli <guyca@users.noreply.github.com>
2018-10-23 14:31:32 +0300

    Update BoolParser.java

--- lib/android/app/src/main/java/com/reactnativenavigation/parse/parsers/BoolParser.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/parse/parsers/BoolParser.java
@@ -1,14 +1,12 @@
 package com.reactnativenavigation.parse.parsers;
 
-import android.support.annotation.NonNull;
-
 import com.reactnativenavigation.parse.params.Bool;
 import com.reactnativenavigation.parse.params.NullBool;
 
 import org.json.JSONObject;
 
 public class BoolParser {
-    public static @NonNull Bool parse(@NonNull JSONObject json, @NonNull String bool) {
+    public static @NonNull Bool parse(JSONObject json, String bool) {
         return json.has(bool) ? new Bool(json.optBoolean(bool)) : new NullBool();
     }
 }
