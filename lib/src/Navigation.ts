c12e260b1f4c087ebbf04cf3f866e8899f048e99
ShaMan123 <shacharnen@gmail.com>
2018-10-26 23:24:13 +0300

    Update lib/src/Navigation.ts
    remove async option => defer to chaining

--- lib/src/Navigation.ts
+++ lib/src/Navigation.ts
@@ -167,8 +167,8 @@ export class Navigation {
     /**
    * Get children ids of stack
    */
-    public async getStackChildrenIds(componentId: string): Promise<any> {
-        return await this.commands.getStackChildrenIds(componentId);
+    public getStackChildrenIds(componentId: string): Promise<any> {
+        return this.commands.getStackChildrenIds(componentId);
     }
 
   /**
