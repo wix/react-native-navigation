c824729996e1cff6731a0adf4f37e6df3db11c97
ShaMan123 <shacharnen@gmail.com>
2018-10-28 08:22:03 +0200

    fix async option

--- lib/src/commands/Commands.ts
+++ lib/src/commands/Commands.ts
@@ -149,9 +149,9 @@ export class Commands {
     return result;
     }
 
-    public getStackChildrenIds(componentId: string) {
+    public async getStackChildrenIds(componentId: string) {
         const commandId = this.uniqueIdProvider.generate('getStackChildrenIds');
-        const result = this.nativeCommandsSender.getStackChildrenIds(commandId, componentId);
+        const result = await this.nativeCommandsSender.getStackChildrenIds(commandId, componentId);
         this.commandsObserver.notify('getStackChildrenIds', { commandId, componentId });
         return result;
     }
