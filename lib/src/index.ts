630539e118bb322373afa443186a9238bc11bac5
ShaMan123 <shacharnen@gmail.com>
2018-10-28 09:34:17 +0200

    Revert "Update lib/src/index.ts"
    This reverts commit 8a3d0aacc9b7ec4bd724bf7088bc8a50a569f1b2.

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -2,8 +2,4 @@ import { Navigation as NavigationClass } from './Navigation';
 
 const singleton = new NavigationClass();
 
-const Navigation = singleton;
-export {
-    Navigation,
-    NavigationClass
-};
+export const Navigation = singleton;
