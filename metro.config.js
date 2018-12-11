b8b05fc25635b28456200201ac1f633fd4122b83
Guy Carmeli <guyc@wix.com>
2018-12-11 11:56:12 +0200

    now?

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   projectRoot: `${__dirname}/playground`,
-  watchFolders: [__dirname],
+  watchFolders: [
+    `${__dirname}/playground`,
+    __dirname
+  ],
   transformer: {
     babelTransformerPath: require.resolve('react-native-typescript-transformer'),
-  },
-  resolver: {
-    extraNodeModules: [__dirname]
   }
 };
 
