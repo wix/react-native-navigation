e552945c9b956c8d4dad290d293c244941382388
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-12-25 22:51:24 +0200

    Add types

--- lib/src/adapters/Element.tsx
+++ lib/src/adapters/Element.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { requireNativeComponent } from 'react-native';
 
-export class Element extends React.Component<{ elementId: any; resizeMode?: any }> {
+export class Element extends React.Component<{ elementId: string; resizeMode?: string }> {
   static propTypes = {
     elementId: PropTypes.string.isRequired,
     resizeMode: PropTypes.string
