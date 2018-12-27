8415548c73d0a60ab1c0a31ff308188537eacef3
henrikra <henrik.raitasola@gmail.com>
2018-12-27 19:52:56 +0200

    Format

--- lib/src/commands/Commands.test.ts
+++ lib/src/commands/Commands.test.ts
@@ -13,7 +13,6 @@ import { OptionsProcessor } from './OptionsProcessor';
 describe('Commands', () => {
   let uut: Commands;
   let mockedNativeCommandsSender: NativeCommandsSender;
-  let nativeCommandsSender: NativeCommandsSender;
   let store: Store;
   let commandsObserver: CommandsObserver;
 
@@ -21,13 +20,12 @@ describe('Commands', () => {
     store = new Store();
     commandsObserver = new CommandsObserver(new UniqueIdProvider());
     mockedNativeCommandsSender = mock(NativeCommandsSender);
-    nativeCommandsSender = instance(mockedNativeCommandsSender);
 
     const mockedOptionsProcessor = mock(OptionsProcessor);
     const optionsProcessor = instance(mockedOptionsProcessor);
 
     uut = new Commands(
-      nativeCommandsSender,
+      instance(mockedNativeCommandsSender),
       new LayoutTreeParser(new UniqueIdProvider()),
       new LayoutTreeCrawler(store, optionsProcessor),
       commandsObserver,
@@ -45,20 +43,25 @@ describe('Commands', () => {
           }
         }
       });
-      verify(mockedNativeCommandsSender.setRoot('setRoot+UNIQUE_ID', deepEqual({
-        root: {
-          type: 'Component',
-          id: 'Component+UNIQUE_ID',
-          children: [],
-          data: {
-            name: 'com.example.MyScreen',
-            options: {},
-            passProps: undefined
-          }
-        },
-        modals: [],
-        overlays: []
-      }))).called();
+      verify(
+        mockedNativeCommandsSender.setRoot(
+          'setRoot+UNIQUE_ID',
+          deepEqual({
+            root: {
+              type: 'Component',
+              id: 'Component+UNIQUE_ID',
+              children: [],
+              data: {
+                name: 'com.example.MyScreen',
+                options: {},
+                passProps: undefined
+              }
+            },
+            modals: [],
+            overlays: []
+          })
+        )
+      ).called();
     });
 
     it('passProps into components', () => {
@@ -72,7 +75,9 @@ describe('Commands', () => {
     });
 
     it('returns a promise with the resolved layout', async () => {
-      when(mockedNativeCommandsSender.setRoot(anything(), anything())).thenResolve('the resolved layout' as any);
+      when(mockedNativeCommandsSender.setRoot(anything(), anything())).thenResolve(
+        'the resolved layout' as any
+      );
       const result = await uut.setRoot({ root: { component: { name: 'com.example.MyScreen' } } });
       expect(result).toEqual('the resolved layout');
     });
@@ -99,50 +104,56 @@ describe('Commands', () => {
           }
         ]
       });
-      verify(mockedNativeCommandsSender.setRoot('setRoot+UNIQUE_ID', deepEqual({
-        root:
-          {
-            type: 'Component',
-            id: 'Component+UNIQUE_ID',
-            children: [],
-            data: {
-              name: 'com.example.MyScreen',
-              options: {},
-              passProps: undefined
-            }
-          },
-        modals: [
-          {
-            type: 'Component',
-            id: 'Component+UNIQUE_ID',
-            children: [],
-            data: {
-              name: 'com.example.MyModal',
-              options: {},
-              passProps: undefined
-            }
-          }
-        ],
-        overlays: [
-          {
-            type: 'Component',
-            id: 'Component+UNIQUE_ID',
-            children: [],
-            data: {
-              name: 'com.example.MyOverlay',
-              options: {},
-              passProps: undefined
-            }
-          }
-        ]
-      }))).called();
+      verify(
+        mockedNativeCommandsSender.setRoot(
+          'setRoot+UNIQUE_ID',
+          deepEqual({
+            root: {
+              type: 'Component',
+              id: 'Component+UNIQUE_ID',
+              children: [],
+              data: {
+                name: 'com.example.MyScreen',
+                options: {},
+                passProps: undefined
+              }
+            },
+            modals: [
+              {
+                type: 'Component',
+                id: 'Component+UNIQUE_ID',
+                children: [],
+                data: {
+                  name: 'com.example.MyModal',
+                  options: {},
+                  passProps: undefined
+                }
+              }
+            ],
+            overlays: [
+              {
+                type: 'Component',
+                id: 'Component+UNIQUE_ID',
+                children: [],
+                data: {
+                  name: 'com.example.MyOverlay',
+                  options: {},
+                  passProps: undefined
+                }
+              }
+            ]
+          })
+        )
+      ).called();
     });
   });
 
   describe('mergeOptions', () => {
     it('passes options for component', () => {
       uut.mergeOptions('theComponentId', { title: '1' } as any);
-      verify(mockedNativeCommandsSender.mergeOptions('theComponentId', deepEqual({title: '1'}))).called();
+      verify(
+        mockedNativeCommandsSender.mergeOptions('theComponentId', deepEqual({ title: '1' }))
+      ).called();
     });
   });
 
@@ -153,16 +164,21 @@ describe('Commands', () => {
           name: 'com.example.MyScreen'
         }
       });
-      verify(mockedNativeCommandsSender.showModal('showModal+UNIQUE_ID', deepEqual({
-        type: 'Component',
-        id: 'Component+UNIQUE_ID',
-        data: {
-          name: 'com.example.MyScreen',
-          options: {},
-          passProps: undefined
-        },
-        children: []
-      }))).called();
+      verify(
+        mockedNativeCommandsSender.showModal(
+          'showModal+UNIQUE_ID',
+          deepEqual({
+            type: 'Component',
+            id: 'Component+UNIQUE_ID',
+            data: {
+              name: 'com.example.MyScreen',
+              options: {},
+              passProps: undefined
+            },
+            children: []
+          })
+        )
+      ).called();
     });
 
     it('passProps into components', () => {
@@ -178,7 +194,9 @@ describe('Commands', () => {
     });
 
     it('returns a promise with the resolved layout', async () => {
-      when(mockedNativeCommandsSender.showModal(anything(), anything())).thenResolve('the resolved layout' as any);
+      when(mockedNativeCommandsSender.showModal(anything(), anything())).thenResolve(
+        'the resolved layout' as any
+      );
       const result = await uut.showModal({ component: { name: 'com.example.MyScreen' } });
       expect(result).toEqual('the resolved layout');
     });
@@ -187,11 +205,19 @@ describe('Commands', () => {
   describe('dismissModal', () => {
     it('sends command to native', () => {
       uut.dismissModal('myUniqueId', {});
-      verify(mockedNativeCommandsSender.dismissModal('dismissModal+UNIQUE_ID', 'myUniqueId', deepEqual({}))).called();
+      verify(
+        mockedNativeCommandsSender.dismissModal(
+          'dismissModal+UNIQUE_ID',
+          'myUniqueId',
+          deepEqual({})
+        )
+      ).called();
     });
 
     it('returns a promise with the id', async () => {
-      when(mockedNativeCommandsSender.dismissModal(anyString(), anything(), anything())).thenResolve('the id' as any);
+      when(
+        mockedNativeCommandsSender.dismissModal(anyString(), anything(), anything())
+      ).thenResolve('the id' as any);
       const result = await uut.dismissModal('myUniqueId');
       expect(result).toEqual('the id');
     });
@@ -200,11 +226,15 @@ describe('Commands', () => {
   describe('dismissAllModals', () => {
     it('sends command to native', () => {
       uut.dismissAllModals({});
-      verify(mockedNativeCommandsSender.dismissAllModals('dismissAllModals+UNIQUE_ID', deepEqual({}))).called();
+      verify(
+        mockedNativeCommandsSender.dismissAllModals('dismissAllModals+UNIQUE_ID', deepEqual({}))
+      ).called();
     });
 
     it('returns a promise with the id', async () => {
-      when(mockedNativeCommandsSender.dismissAllModals(anyString(), anything())).thenResolve('the id' as any);
+      when(mockedNativeCommandsSender.dismissAllModals(anyString(), anything())).thenResolve(
+        'the id' as any
+      );
       const result = await uut.dismissAllModals();
       expect(result).toEqual('the id');
     });
@@ -212,23 +242,33 @@ describe('Commands', () => {
 
   describe('push', () => {
     it('resolves with the parsed layout', async () => {
-      when(mockedNativeCommandsSender.push(anyString(), anyString(), anything())).thenResolve('the resolved layout' as any);
-      const result = await uut.push('theComponentId', { component: { name: 'com.example.MyScreen' } });
+      when(mockedNativeCommandsSender.push(anyString(), anyString(), anything())).thenResolve(
+        'the resolved layout' as any
+      );
+      const result = await uut.push('theComponentId', {
+        component: { name: 'com.example.MyScreen' }
+      });
       expect(result).toEqual('the resolved layout');
     });
 
     it('parses into correct layout node and sends to native', () => {
       uut.push('theComponentId', { component: { name: 'com.example.MyScreen' } });
-      verify(mockedNativeCommandsSender.push('push+UNIQUE_ID', 'theComponentId', deepEqual({
-        type: 'Component',
-        id: 'Component+UNIQUE_ID',
-        data: {
-          name: 'com.example.MyScreen',
-          options: {},
-          passProps: undefined
-        },
-        children: []
-      }))).called();
+      verify(
+        mockedNativeCommandsSender.push(
+          'push+UNIQUE_ID',
+          'theComponentId',
+          deepEqual({
+            type: 'Component',
+            id: 'Component+UNIQUE_ID',
+            data: {
+              name: 'com.example.MyScreen',
+              options: {},
+              passProps: undefined
+            },
+            children: []
+          })
+        )
+      ).called();
     });
 
     it('calls component generator once', async () => {
@@ -244,13 +284,22 @@ describe('Commands', () => {
   describe('pop', () => {
     it('pops a component, passing componentId', () => {
       uut.pop('theComponentId', {});
-      verify(mockedNativeCommandsSender.pop('pop+UNIQUE_ID', 'theComponentId', deepEqual({}))).called();
+      verify(
+        mockedNativeCommandsSender.pop('pop+UNIQUE_ID', 'theComponentId', deepEqual({}))
+      ).called();
     });
     it('pops a component, passing componentId and options', () => {
       const options = {
         customTransition: {
           animations: [
-            { type: 'sharedElement', fromId: 'title2', toId: 'title1', startDelay: 0, springVelocity: 0.2, duration: 0.5 }
+            {
+              type: 'sharedElement',
+              fromId: 'title2',
+              toId: 'title1',
+              startDelay: 0,
+              springVelocity: 0.2,
+              duration: 0.5
+            }
           ],
           duration: 0.8
         }
@@ -260,7 +309,9 @@ describe('Commands', () => {
     });
 
     it('pop returns a promise that resolves to componentId', async () => {
-      when(mockedNativeCommandsSender.pop(anyString(), anyString(), anything())).thenResolve('theComponentId' as any);
+      when(mockedNativeCommandsSender.pop(anyString(), anyString(), anything())).thenResolve(
+        'theComponentId' as any
+      );
       const result = await uut.pop('theComponentId', {});
       expect(result).toEqual('theComponentId');
     });
@@ -269,11 +320,15 @@ describe('Commands', () => {
   describe('popTo', () => {
     it('pops all components until the passed Id is top', () => {
       uut.popTo('theComponentId', {});
-      verify(mockedNativeCommandsSender.popTo('popTo+UNIQUE_ID', 'theComponentId', deepEqual({}))).called();
+      verify(
+        mockedNativeCommandsSender.popTo('popTo+UNIQUE_ID', 'theComponentId', deepEqual({}))
+      ).called();
     });
 
     it('returns a promise that resolves to targetId', async () => {
-      when(mockedNativeCommandsSender.popTo(anyString(), anyString(), anything())).thenResolve('theComponentId' as any);
+      when(mockedNativeCommandsSender.popTo(anyString(), anyString(), anything())).thenResolve(
+        'theComponentId' as any
+      );
       const result = await uut.popTo('theComponentId');
       expect(result).toEqual('theComponentId');
     });
@@ -282,11 +337,15 @@ describe('Commands', () => {
   describe('popToRoot', () => {
     it('pops all components to root', () => {
       uut.popToRoot('theComponentId', {});
-      verify(mockedNativeCommandsSender.popToRoot('popToRoot+UNIQUE_ID', 'theComponentId', deepEqual({}))).called();
+      verify(
+        mockedNativeCommandsSender.popToRoot('popToRoot+UNIQUE_ID', 'theComponentId', deepEqual({}))
+      ).called();
     });
 
     it('returns a promise that resolves to targetId', async () => {
-      when(mockedNativeCommandsSender.popToRoot(anyString(), anyString(), anything())).thenResolve('theComponentId' as any);
+      when(mockedNativeCommandsSender.popToRoot(anyString(), anyString(), anything())).thenResolve(
+        'theComponentId' as any
+      );
       const result = await uut.popToRoot('theComponentId');
       expect(result).toEqual('theComponentId');
     });
@@ -295,18 +354,24 @@ describe('Commands', () => {
   describe('setStackRoot', () => {
     it('parses into correct layout node and sends to native', () => {
       uut.setStackRoot('theComponentId', [{ component: { name: 'com.example.MyScreen' } }]);
-      verify(mockedNativeCommandsSender.setStackRoot('setStackRoot+UNIQUE_ID', 'theComponentId', deepEqual([
-        {
-          type: 'Component',
-          id: 'Component+UNIQUE_ID',
-          data: {
-            name: 'com.example.MyScreen',
-            options: {},
-            passProps: undefined
-          },
-          children: []
-        }
-      ]))).called();
+      verify(
+        mockedNativeCommandsSender.setStackRoot(
+          'setStackRoot+UNIQUE_ID',
+          'theComponentId',
+          deepEqual([
+            {
+              type: 'Component',
+              id: 'Component+UNIQUE_ID',
+              data: {
+                name: 'com.example.MyScreen',
+                options: {},
+                passProps: undefined
+              },
+              children: []
+            }
+          ])
+        )
+      ).called();
     });
   });
 
@@ -317,20 +382,27 @@ describe('Commands', () => {
           name: 'com.example.MyScreen'
         }
       });
-      verify(mockedNativeCommandsSender.showOverlay('showOverlay+UNIQUE_ID', deepEqual({
-        type: 'Component',
-        id: 'Component+UNIQUE_ID',
-        data: {
-          name: 'com.example.MyScreen',
-          options: {},
-          passProps: undefined
-        },
-        children: []
-      }))).called();
+      verify(
+        mockedNativeCommandsSender.showOverlay(
+          'showOverlay+UNIQUE_ID',
+          deepEqual({
+            type: 'Component',
+            id: 'Component+UNIQUE_ID',
+            data: {
+              name: 'com.example.MyScreen',
+              options: {},
+              passProps: undefined
+            },
+            children: []
+          })
+        )
+      ).called();
     });
 
     it('resolves with the component id', async () => {
-      when(mockedNativeCommandsSender.showOverlay(anyString(), anything())).thenResolve('Component1' as any);
+      when(mockedNativeCommandsSender.showOverlay(anyString(), anything())).thenResolve(
+        'Component1' as any
+      );
       const result = await uut.showOverlay({ component: { name: 'com.example.MyScreen' } });
       expect(result).toEqual('Component1');
     });
@@ -338,7 +410,9 @@ describe('Commands', () => {
 
   describe('dismissOverlay', () => {
     it('check promise returns true', async () => {
-      when(mockedNativeCommandsSender.dismissOverlay(anyString(), anyString())).thenResolve(true as any);
+      when(mockedNativeCommandsSender.dismissOverlay(anyString(), anyString())).thenResolve(
+        true as any
+      );
       const result = await uut.dismissOverlay('Component1');
       verify(mockedNativeCommandsSender.dismissOverlay(anyString(), anyString())).called();
       expect(result).toEqual(true);
@@ -346,7 +420,9 @@ describe('Commands', () => {
 
     it('send command to native with componentId', () => {
       uut.dismissOverlay('Component1');
-      verify(mockedNativeCommandsSender.dismissOverlay('dismissOverlay+UNIQUE_ID', 'Component1')).called();
+      verify(
+        mockedNativeCommandsSender.dismissOverlay('dismissOverlay+UNIQUE_ID', 'Component1')
+      ).called();
     });
   });
 
@@ -418,7 +494,10 @@ describe('Commands', () => {
         getLaunchArgs: ['id']
       };
       const paramsForMethodName: Record<string, object> = {
-        setRoot: { commandId: 'setRoot+UNIQUE_ID', layout: { root: 'parsed', modals: [], overlays: [] } },
+        setRoot: {
+          commandId: 'setRoot+UNIQUE_ID',
+          layout: { root: 'parsed', modals: [], overlays: [] }
+        },
         setDefaultOptions: { options: {} },
         mergeOptions: { componentId: 'id', options: {} },
         showModal: { commandId: 'showModal+UNIQUE_ID', layout: 'parsed' },
@@ -428,10 +507,14 @@ describe('Commands', () => {
         pop: { commandId: 'pop+UNIQUE_ID', componentId: 'id', mergeOptions: {} },
         popTo: { commandId: 'popTo+UNIQUE_ID', componentId: 'id', mergeOptions: {} },
         popToRoot: { commandId: 'popToRoot+UNIQUE_ID', componentId: 'id', mergeOptions: {} },
-        setStackRoot: { commandId: 'setStackRoot+UNIQUE_ID', componentId: 'id', layout: ['parsed'] },
+        setStackRoot: {
+          commandId: 'setStackRoot+UNIQUE_ID',
+          componentId: 'id',
+          layout: ['parsed']
+        },
         showOverlay: { commandId: 'showOverlay+UNIQUE_ID', layout: 'parsed' },
         dismissOverlay: { commandId: 'dismissOverlay+UNIQUE_ID', componentId: 'id' },
-        getLaunchArgs: { commandId: 'getLaunchArgs+UNIQUE_ID' },
+        getLaunchArgs: { commandId: 'getLaunchArgs+UNIQUE_ID' }
       };
       _.forEach(getAllMethodsOfUut(), (m) => {
         it(`for ${m}`, () => {
