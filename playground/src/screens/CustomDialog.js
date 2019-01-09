19b0ad1e6c2a0f20576966cc1c46278531e313b7
eladgel <elad@honeybook.com>
2019-01-09 18:07:57 +0200

    Update CustomDialog.js

--- playground/src/screens/CustomDialog.js
+++ playground/src/screens/CustomDialog.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const { PureComponent } = require('react');
 
-const { Text, Button, View, Alert, Platform ,ScrollView} = require('react-native');
+const { Text, Button, View, Alert, Platform } = require('react-native');
 const { Navigation } = require('react-native-navigation');
 
 const testIDs = require('../testIDs');
@@ -15,17 +15,13 @@ class CustomDialog extends PureComponent {
 
   render() {
     return (
-        <View style={styles.container}>
-        <ScrollView style={styles.root}>
-        <View style={{height: 60, backgroundColor: 'red'}}/>
-    <View style={{height: 60, backgroundColor: 'green'}}/>
-    <View style={{height: 60, backgroundColor: 'red'}}/>
-    <View style={{height: 60, backgroundColor: 'green'}}/>
-    <View style={{height: 60, backgroundColor: 'red'}}/>
-    <View style={{height: 60, backgroundColor: 'green'}}/>
-    </ScrollView>
-    </View>
-  );
+      <View style={styles.root}>
+        <Text style={styles.h1} testID={testIDs.DIALOG_HEADER}>Test view</Text>
+        <Button title='OK' testID={testIDs.OK_BUTTON} onPress={() => this.onCLickOk()} />
+        <Button title='Set Root' testID={testIDs.SET_ROOT_BUTTON} onPress={() => this.onCLickSetRoot()} />
+        <Button title='Set Intercept touch' testID={testIDs.SET_INTERCEPT_TOUCH} onPress={() => this.onCLickSetInterceptTouch()} />
+      </View>
+    );
   }
 
   didDisappear() {
@@ -59,17 +55,14 @@ class CustomDialog extends PureComponent {
 
 const styles = {
   root: {
-    width: 400,
-    height: 200,
-  },
-  container: {
-
-    width: 400,
-    height: 200,
-    flexDirection: 'column',
+    backgroundColor: 'green',
     justifyContent: 'center',
     alignItems: 'center',
-    alignSelf: 'center'
+    height: 140,
+    bottom: 0,
+    position: 'absolute',
+    left: 0,
+    right: 0
   },
   h1: {
     fontSize: 24,
