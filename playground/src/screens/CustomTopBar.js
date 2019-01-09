e4c9310bd562ea0284fa19403354c7bf809de95f
eladgel <elad@honeybook.com>
2019-01-09 17:39:37 +0200

    scrollview is not scrollable when using overlay in android

--- playground/src/screens/CustomTopBar.js
+++ playground/src/screens/CustomTopBar.js
@@ -1,62 +1,76 @@
+import {ScrollView} from "react-native";
+
 const React = require('react');
 const { Component } = require('react');
 const {
-  StyleSheet,
-  View,
-  TouchableOpacity,
-  Text,
-  Alert,
-  Platform
+    StyleSheet,
+    View,
+    TouchableOpacity,
+    Text,
+    Alert,
+    Platform
 } = require('react-native');
 const { Navigation } = require('react-native-navigation');
 
 class CustomTopBar extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.subscription = Navigation.events().bindComponent(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.subscription = Navigation.events().bindComponent(this);
+    }
 
-  componentDidAppear() {
-    console.log('RNN', 'CTB.componentDidAppear');
-  }
+    componentDidAppear() {
+        console.log('RNN', 'CTB.componentDidAppear');
+    }
 
-  componentDidDisappear() {
-    console.log('RNN', `CTB.componentDidDisappear`);
-  }
+    componentDidDisappear() {
+        console.log('RNN', `CTB.componentDidDisappear`);
+    }
 
-  componentDidMount() {
-    console.log('RNN', `CTB.componentDidMount`);
-  }
+    componentDidMount() {
+        console.log('RNN', `CTB.componentDidMount`);
+    }
 
-  componentWillUnmount() {
-    console.log('RNN', `CTB.componentWillUnmount`);
-    this.subscription.remove();
-  }
+    componentWillUnmount() {
+        console.log('RNN', `CTB.componentWillUnmount`);
+        this.subscription.remove();
+    }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={() => Alert.alert(this.props.title, 'Thanks for that :)')}>
-          <Text style={styles.text}>Press Me</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <ScrollView style={styles.root}>
+                <View style={{height:60,backgroundColor: 'red'}}/>
+                <View style={{height:60,backgroundColor: 'green'}}/>
+                <View style={{height:60,backgroundColor: 'red'}}/>
+                <View style={{height:60,backgroundColor: 'green'}}/>
+                <View style={{height:60,backgroundColor: 'red'}}/>
+                <View style={{height:60,backgroundColor: 'green'}}/>
+            </ScrollView>
+            </View>
+        );
+    }
 }
 
 module.exports = CustomTopBar;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignSelf: 'center'
-  },
-  text: {
-    alignSelf: 'center',
-    color: 'black',
-  }
+    root:{
+      width:400,
+      height:200,
+    },
+    container: {
+
+        width:400,
+        height:200,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center'
+    },
+    text: {
+        alignSelf: 'center',
+        color: 'black',
+    }
 });
