c23ca3ef06479ef5cef215fd82bb5e4ebe9610b9
Jérémy M <jeremy.magrin@epitech.eu>
2019-02-04 10:40:33 +0100

    Fix mergeOptions bottomTab iOS

--- lib/ios/RNNBasePresenter.m
+++ lib/ios/RNNBasePresenter.m
@@ -17,9 +17,6 @@
 	if ((options.bottomTab.text.hasValue || options.bottomTab.icon.hasValue || options.bottomTab.selectedIcon.hasValue)) {
 		UITabBarItem* tabItem = [RNNTabBarItemCreator updateTabBarItem:viewController.tabBarItem bottomTabOptions:options.bottomTab];
 		viewController.tabBarItem = tabItem;
-		[options.bottomTab.text consume];
-		[options.bottomTab.icon consume];
-		[options.bottomTab.selectedIcon consume];
 	}
 }
 
@@ -35,6 +32,12 @@
 	UIViewController* viewController = self.bindedViewController;
 	if (newOptions.bottomTab.badge.hasValue && [viewController.parentViewController isKindOfClass:[UITabBarController class]]) {
 		[viewController rnn_setTabBarItemBadge:newOptions.bottomTab.badge.get];
+	}
+	
+	if ((newOptions.bottomTab.text.hasValue || newOptions.bottomTab.icon.hasValue || newOptions.bottomTab.selectedIcon.hasValue)) {
+		RNNNavigationOptions* buttonsResolvedOptions = [(RNNNavigationOptions *)[currentOptions overrideOptions:newOptions] withDefault:defaultOptions];
+		UITabBarItem* tabItem = [RNNTabBarItemCreator updateTabBarItem:viewController.tabBarItem bottomTabOptions:buttonsResolvedOptions.bottomTab];
+		viewController.tabBarItem = tabItem;
 	}
 }
 
