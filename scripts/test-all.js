13574f743780aee32f79e23c6b6b4a0da032ec69
Guy Carmeli <guyc@wix.com>
2019-04-30 15:22:40 +0300

    Download licenses before unit and e2e tests

--- scripts/test-all.js
+++ scripts/test-all.js
@@ -4,6 +4,12 @@ const exec = require('shell-utils').exec;
 async function run() {
   exec.execSync(`npm run clean`);
   exec.execSync(`npm run test-js`);
+
+  if (process.env.JENKINS_CI) {
+    const sdkmanager = '/usr/local/share/android-sdk/tools/bin/sdkmanager';
+    exec.execSync(`echo y | ${sdkmanager} --update && echo y | ${sdkmanager} --licenses`);
+}
+
   exec.execAsyncSilent(`npm run start`);
   await exec.execAsyncAll(`npm run test-unit-android`, `npm run test-unit-ios`);
   await exec.execAsyncAll(`npm run test-e2e-android`, `npm run test-e2e-ios`);
