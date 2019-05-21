c3a0c7c7e75d0924193e22d0891ea6c7532070ce
Eric Lewis <ericlewis777@gmail.com>
2019-05-21 11:06:27 -0400

    feature flag fabric

--- lib/ios/RNNReactView.h
+++ lib/ios/RNNReactView.h
@@ -1,9 +1,18 @@
+#ifdef RN_FABRIC_ENABLED
 #import <React/RCTFabricSurfaceHostingProxyRootView.h>
+#else
+#import <React/RCTRootView.h>
+#endif
+
 #import <React/RCTRootViewDelegate.h>
 
 typedef void (^RNNReactViewReadyCompletionBlock)(void);
 
+#ifdef RN_FABRIC_ENABLED
 @interface RNNReactView : RCTFabricSurfaceHostingProxyRootView <RCTRootViewDelegate>
+#else
+@interface RNNReactView : RCTRootView <RCTRootViewDelegate>
+#endif
 
 - (instancetype)initWithBridge:(RCTBridge *)bridge moduleName:(NSString *)moduleName initialProperties:(NSDictionary *)initialProperties availableSize:(CGSize)availableSize reactViewReadyBlock:(RNNReactViewReadyCompletionBlock)reactViewReadyBlock;
 
