98c0a71cb5873a9d3faa06576f6e18cffd5c0b10
郑成功 <zhengcg@many-it.com>
2019-06-16 12:13:22 +0800

    feat: add support to load icon from server

--- android/app/src/main/java/com/reactnativenavigation/react/ImageLoader.java
+++ android/app/src/main/java/com/reactnativenavigation/react/ImageLoader.java
@@ -5,6 +5,14 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.reactnativenavigation.NavigationApplication;
+
+import java.io.IOException;
+import java.net.URL;
 
 import com.reactnativenavigation.NavigationApplication;
 
@@ -18,6 +26,9 @@ public class ImageLoader {
             Uri uri = Uri.parse(iconSource);
             if (isLocalFile(uri)) {
                 return loadFile(uri);
+            } else if (uri.toString().startsWith("http")
+             || uri.toString().startsWith("https")) {
+                return loadIcon(uri.toString());
             } else {
                 return loadResource(iconSource);
             }
@@ -35,5 +46,26 @@ public class ImageLoader {
 
     private static Drawable loadResource(String iconSource) {
         return ResourceDrawableIdHelper.instance.getResourceDrawable(NavigationApplication.instance, iconSource);
+    }
+    private static Drawable loadIcon(String iconSource) {
+        try {
+            StrictMode.ThreadPolicy threadPolicy = StrictMode.getThreadPolicy();
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+
+            Drawable drawable = tryLoadIcon(iconSource);
+
+            StrictMode.setThreadPolicy(threadPolicy);
+            return drawable;
+        } catch (Exception e) {
+            Log.e("loadTabIcon", "Unable to load icon: " + iconSource);
+            return new BitmapDrawable();
+        }
+    }
+
+    @NonNull
+    private static Drawable tryLoadIcon(String iconSource) throws IOException {
+        URL url = new URL(iconSource);
+        Bitmap bitmap = BitmapFactory.decodeStream(url.openStream());
+        return new BitmapDrawable(NavigationApplication.instance.getResources(), bitmap);
     }
 }
