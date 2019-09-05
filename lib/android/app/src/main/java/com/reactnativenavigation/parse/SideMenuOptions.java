88a1f598825f4c357e79d0ce7e38a0a75126ad15
Royce Townsend <git@roycetownsend.com>
2019-09-05 11:54:31 +0800

    [Android] Fix/Avoid issue with sidemenu:enabled flag being clobbered.

--- lib/android/app/src/main/java/com/reactnativenavigation/parse/SideMenuOptions.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/parse/SideMenuOptions.java
@@ -38,7 +38,6 @@ public class SideMenuOptions {
     }
 
     public void mergeWithDefault(SideMenuOptions defaultOptions) {
-        if (!visible.hasValue()) visible = defaultOptions.visible;
         if (!animate.hasValue()) animate = defaultOptions.animate;
         if (!enabled.hasValue()) enabled = defaultOptions.enabled;
         if (!height.hasValue()) height = defaultOptions.height;
