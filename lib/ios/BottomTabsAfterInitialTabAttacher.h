f5da1fb78153c70e71cebf9186fa114fa3e7380f
Yogev Ben David <yogev132@gmail.com>
2019-12-09 11:44:17 +0200

    Update BottomTabsAfterInitialTabAttacher.h

--- lib/ios/BottomTabsAfterInitialTabAttacher.h
+++ lib/ios/BottomTabsAfterInitialTabAttacher.h
@@ -1,17 +1,5 @@
-//
-//  BottomTabsAfterInitialTabAttacher.h
-//  ReactNativeNavigation
-//
-//  Created by Yogev Ben David on 08/12/2019.
-//  Copyright © 2019 Wix. All rights reserved.
-//
-
 #import "BottomTabsBaseAttacher.h"
-
-NS_ASSUME_NONNULL_BEGIN
 
 @interface BottomTabsAfterInitialTabAttacher : BottomTabsBaseAttacher
 
 @end
-
-NS_ASSUME_NONNULL_END
