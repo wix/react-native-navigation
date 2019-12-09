8bc54b1835c30e547bddc19ff17b52e7461fc206
Yogev Ben David <yogev132@gmail.com>
2019-12-09 11:45:14 +0200

    Update BottomTabsTogetherAttacher.h

--- lib/ios/BottomTabsTogetherAttacher.h
+++ lib/ios/BottomTabsTogetherAttacher.h
@@ -1,17 +1,5 @@
-//
-//  BottomTabsTogetherAttacher.h
-//  ReactNativeNavigation
-//
-//  Created by Yogev Ben David on 08/12/2019.
-//  Copyright © 2019 Wix. All rights reserved.
-//
-
 #import "BottomTabsBaseAttacher.h"
-
-NS_ASSUME_NONNULL_BEGIN
 
 @interface BottomTabsTogetherAttacher : BottomTabsBaseAttacher
 
 @end
-
-NS_ASSUME_NONNULL_END
