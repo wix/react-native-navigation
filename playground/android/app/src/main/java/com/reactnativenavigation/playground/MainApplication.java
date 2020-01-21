7d253e944ad64c9c3ea709f3c56bed98c15a666c
yogevbd <yogev132@gmail.com>
2020-01-21 16:28:57 +0200

    Align MainApplication with RN

--- playground/android/app/src/main/java/com/reactnativenavigation/playground/MainApplication.java
+++ playground/android/app/src/main/java/com/reactnativenavigation/playground/MainApplication.java
@@ -3,15 +3,38 @@ package com.reactnativenavigation.playground;
 import com.entria.views.RNViewOverflowPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+
+import com.facebook.react.shell.MainReactPackage;
 import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationPackage;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import androidx.annotation.Nullable;
 
 public class MainApplication extends NavigationApplication {
+
+    private final ReactNativeHost mReactNativeHost =
+            new ReactNativeHost(this) {
+                @Override
+                protected String getJSMainModuleName() {
+                    return "index";
+                }
+
+                @Override
+                public boolean getUseDeveloperSupport() {
+                    return BuildConfig.DEBUG;
+                }
+
+                @Override
+                public List<ReactPackage> getPackages() {
+                    return Arrays.asList(new MainReactPackage(), new NavigationPackage(mReactNativeHost));
+                }
+            };
+
 
     @Override
     public void onCreate() {
@@ -20,25 +43,7 @@ public class MainApplication extends NavigationApplication {
     }
 
     @Override
-    protected ReactNativeHost createReactNativeHost() {
-        return new NavigationReactNativeHost(this) {
-            @Override
-            protected String getJSMainModuleName() {
-                return "index";
-            }
-        };
-    }
-
-    @Override
-    public boolean isDebug() {
-        return BuildConfig.DEBUG;
-    }
-
-    @Nullable
-    @Override
-    public List<ReactPackage> createAdditionalReactPackages() {
-        List<ReactPackage> packages = new ArrayList<>();
-        packages.add(new RNViewOverflowPackage());
-        return packages;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 }
