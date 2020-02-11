658e27f3ace0c2092d91fe10c94f574ecc3efa14
Piérre Reimertz <reimertz@users.noreply.github.com>
2020-02-11 16:11:43 +0100

    Update SharedElementTransitionOptions.m

--- lib/ios/SharedElementTransitionOptions.m
+++ lib/ios/SharedElementTransitionOptions.m
@@ -9,8 +9,8 @@
 	
 	self.fromId = [transition objectForKey:@"fromId"];
 	self.toId = [transition objectForKey:@"toId"];
-    self.startDelay = [TimeIntervalParser parse:transition key:@"startDelay"];
-    self.duration = [TimeIntervalParser parse:transition key:@"duration"];
+    	self.startDelay = [TimeIntervalParser parse:transition key:@"startDelay"];
+    	self.duration = [TimeIntervalParser parse:transition key:@"duration"];
 	self.interpolation = [TextParser parse:transition key:@"interpolation"];
 	
 	return self;
