a0553be3da33de2d2f6fdcfbc457cfa0db08f56f
Maycon <mayconjcm@gmail.com>
2020-03-07 16:52:05 -0300

    Turn 'build' into a script file + add Windows compatibility.

---
+++ scripts/build.js
@@ -0,0 +1,15 @@
+const exec = require('shell-utils').exec;
+const rimraf = require('rimraf');
+
+const isWindows = process.platform === 'win32' ? true : false;
+
+run();
+
+function run() {
+    if (isWindows) {
+        rimraf.sync(`./lib/dist`);
+        exec.execSync(`tsc`);
+    } else {
+        exec.execSync(`rm -rf ./lib/dist && tsc`);
+    }
+}
