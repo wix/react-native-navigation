14706d71d257db3d35bf1c02489ee9e817c91f0f
Yogev Ben David <yogevbd@wix.com>
2020-07-14 15:42:32 +0300

    Update RNNBridgeManager.mm

--- lib/ios/RNNBridgeManager.mm
+++ lib/ios/RNNBridgeManager.mm
@@ -10,7 +10,7 @@
 #import "RNNEventEmitter.h"
 #import "RNNSplashScreen.h"
 #import "RNNBridgeModule.h"
-#import "RNNRootViewCreator.h"
+#import "RNNComponentViewCreator.h"
 #import "RNNReactRootViewCreator.h"
 #import "RNNReactComponentRegistry.h"
 
@@ -23,13 +23,15 @@
 @property (nonatomic, strong, readwrite) RCTBridge *bridge;
 @property (nonatomic, strong, readwrite) RNNExternalComponentStore *store;
 @property (nonatomic, strong, readwrite) RNNReactComponentRegistry *componentRegistry;
+@property (nonatomic, strong, readonly) RNNOverlayManager *overlayManager;
+@property (nonatomic, strong, readonly) RNNModalManager *modalManager;
 
 @end
 
 @implementation RNNBridgeManager {
 	NSURL* _jsCodeLocation;
 	NSDictionary* _launchOptions;
-	id<RNNBridgeManagerDelegate> _delegate;
+	id<RCTBridgeDelegate> _delegate;
 	RCTBridge* _bridge;
 	UIWindow* _mainWindow;
 	
@@ -38,12 +40,14 @@
 	RNNCommandsHandler* _commandsHandler;
 }
 
-- (instancetype)initWithJsCodeLocation:(NSURL *)jsCodeLocation launchOptions:(NSDictionary *)launchOptions bridgeManagerDelegate:(id<RNNBridgeManagerDelegate>)delegate mainWindow:(UIWindow *)mainWindow {
+- (instancetype)initWithJsCodeLocation:(NSURL *)jsCodeLocation launchOptions:(NSDictionary *)launchOptions bridgeManagerDelegate:(id<RCTBridgeDelegate>)delegate mainWindow:(UIWindow *)mainWindow {
 	if (self = [super init]) {
 		_mainWindow = mainWindow;
 		_jsCodeLocation = jsCodeLocation;
 		_launchOptions = launchOptions;
 		_delegate = delegate;
+		
+		_overlayManager = [RNNOverlayManager new];
 		
 		_store = [RNNExternalComponentStore new];
 		_bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:_launchOptions];
@@ -69,6 +73,10 @@
 	return self;
 }
 
+- (void)setJSCodeLocation:(NSURL *)jsCodeLocation {
+	_jsCodeLocation = jsCodeLocation;
+}
+
 - (void)registerExternalComponent:(NSString *)name callback:(RNNExternalViewCreator)callback {
 	[_store registerExternalComponent:name callback:callback];
 }
@@ -84,17 +92,22 @@
 # pragma mark - RCTBridgeDelegate
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
-	return _jsCodeLocation;
+    if ([_delegate respondsToSelector:@selector(sourceURLForBridge:)]) {
+        return [_delegate sourceURLForBridge:bridge];
+    } else {
+        return _jsCodeLocation;
+    }
 }
 
 - (NSArray<id<RCTBridgeModule>> *)extraModulesForBridge:(RCTBridge *)bridge {
 	RNNEventEmitter *eventEmitter = [[RNNEventEmitter alloc] init];
-
-	id<RNNRootViewCreator> rootViewCreator = [[RNNReactRootViewCreator alloc] initWithBridge:bridge];
+    _modalManager = [[RNNModalManager alloc] initWithBridge:bridge];
+    
+	id<RNNComponentViewCreator> rootViewCreator = [[RNNReactRootViewCreator alloc] initWithBridge:bridge eventEmitter:eventEmitter];
 	_componentRegistry = [[RNNReactComponentRegistry alloc] initWithCreator:rootViewCreator];
-	RNNControllerFactory *controllerFactory = [[RNNControllerFactory alloc] initWithRootViewCreator:rootViewCreator eventEmitter:eventEmitter store:_store componentRegistry:_componentRegistry andBridge:bridge];
-	
-	_commandsHandler = [[RNNCommandsHandler alloc] initWithControllerFactory:controllerFactory eventEmitter:eventEmitter stackManager:[RNNNavigationStackManager new] modalManager:[RNNModalManager new] overlayManager:[RNNOverlayManager new] mainWindow:_mainWindow];
+	RNNControllerFactory *controllerFactory = [[RNNControllerFactory alloc] initWithRootViewCreator:rootViewCreator eventEmitter:eventEmitter store:_store componentRegistry:_componentRegistry andBridge:bridge bottomTabsAttachModeFactory:[BottomTabsAttachModeFactory new]];
+
+	_commandsHandler = [[RNNCommandsHandler alloc] initWithControllerFactory:controllerFactory eventEmitter:eventEmitter modalManager:_modalManager overlayManager:_overlayManager mainWindow:_mainWindow];
 	RNNBridgeModule *bridgeModule = [[RNNBridgeModule alloc] initWithCommandsHandler:_commandsHandler];
 
 	return [@[bridgeModule,eventEmitter] arrayByAddingObjectsFromArray:[self extraModulesFromDelegate]];
@@ -112,7 +125,10 @@
 }
 
 - (void)onBridgeWillReload {
-	UIApplication.sharedApplication.delegate.window.rootViewController =  nil;
+	[_overlayManager dismissAllOverlays];
+	[_modalManager dismissAllModalsSynchronosly];
+	[_componentRegistry clear];
+	UIApplication.sharedApplication.delegate.window.rootViewController = nil;
 }
 
 @end
