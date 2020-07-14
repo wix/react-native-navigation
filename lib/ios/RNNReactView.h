5d1809a7a17cfd984160eceb9a120d774814974a
Yogev Ben David <yogevbd@wix.com>
2020-07-14 15:42:04 +0300

    Update RNNReactView.h

--- lib/ios/RNNReactView.h
+++ lib/ios/RNNReactView.h
@@ -4,21 +4,43 @@
 #import <React/RCTRootView.h>
 #endif
 
+#import "RNNEventEmitter.h"
+#import "UIView+Utils.h"
 #import <React/RCTRootViewDelegate.h>
+#import <React/RCTUIManager.h>
+
+#define ComponentTypeScreen @"Component"
+#define ComponentTypeTitle @"TopBarTitle"
+#define ComponentTypeButton @"TopBarButton"
+#define ComponentTypeBackground @"TopBarBackground"
 
 typedef void (^RNNReactViewReadyCompletionBlock)(void);
 
 #ifdef RN_FABRIC_ENABLED
-@interface RNNReactView : RCTFabricSurfaceHostingProxyRootView <RCTRootViewDelegate>
+@interface RNNReactView
+    : RCTFabricSurfaceHostingProxyRootView <RCTRootViewDelegate>
 #else
 @interface RNNReactView : RCTRootView <RCTRootViewDelegate>
 #endif
 
-- (instancetype)initWithBridge:(RCTBridge *)bridge moduleName:(NSString *)moduleName initialProperties:(NSDictionary *)initialProperties availableSize:(CGSize)availableSize reactViewReadyBlock:(RNNReactViewReadyCompletionBlock)reactViewReadyBlock;
+- (instancetype)initWithBridge:(RCTBridge *)bridge
+                    moduleName:(NSString *)moduleName
+             initialProperties:(NSDictionary *)initialProperties
+                  eventEmitter:(RNNEventEmitter *)eventEmitter
+           reactViewReadyBlock:
+               (RNNReactViewReadyCompletionBlock)reactViewReadyBlock;
 
-@property (nonatomic, copy) void (^rootViewDidChangeIntrinsicSize)(CGSize intrinsicSize);
-@property (nonatomic, copy) RNNReactViewReadyCompletionBlock reactViewReadyBlock;
+@property(nonatomic, copy) RNNReactViewReadyCompletionBlock reactViewReadyBlock;
+@property(nonatomic, strong) RNNEventEmitter *eventEmitter;
 
-- (void)setAlignment:(NSString *)alignment inFrame:(CGRect)frame;
+- (NSString *)componentId;
+
+- (NSString *)componentType;
+
+- (void)componentDidAppear;
+
+- (void)componentDidDisappear;
+
+- (void)invalidate;
 
 @end
