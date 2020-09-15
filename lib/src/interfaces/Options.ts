2cf542a56102cea2f393841a1da1d226f23b8b28
Marc Rousavy <marcrousavy@hotmail.com>
2020-09-15 14:52:48 +0200

    [JS] Update `Interpolation` types

--- lib/src/interfaces/Options.ts
+++ lib/src/interfaces/Options.ts
@@ -60,12 +60,12 @@ type SystemItemIcon =
   | 'undo'
   | 'redo';
 type Interpolation =
-  | 'linear'
-  | 'accelerateDecelerate'
-  | 'decelerate'
-  | 'accelerate'
-  | 'decelerateAccelerate'
-  | 'overshoot';
+  | { mode: 'linear' }
+  | { mode: 'accelerateDecelerate' }
+  | { mode: 'decelerate' }
+  | { mode: 'accelerate' }
+  | { mode: 'decelerateAccelerate' }
+  | { mode: 'overshoot'; tension?: number };
 
 export interface OptionsSplitView {
   /**
