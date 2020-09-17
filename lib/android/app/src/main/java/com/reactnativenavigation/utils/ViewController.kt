288bc639e47a0fe6c72c1467314b23ba158b9dc9
Guy Carmeli <guyc@wix.com>
2020-09-17 22:27:43 +0300

    Actually wait for the appearing view to render before kicking off SET logic

---
+++ lib/android/app/src/main/java/com/reactnativenavigation/utils/ViewController.kt
@@ -0,0 +1,15 @@
+package com.reactnativenavigation.utils
+
+import android.view.View
+import com.reactnativenavigation.viewcontrollers.viewcontroller.ViewController
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+suspend fun ViewController<*>.awaitRender() = suspendCoroutine<Unit> { cont ->
+    addOnAppearedListener(object : Runnable {
+        override fun run() {
+            removeOnAppearedListener(this)
+            cont.resume(Unit)
+        }
+    })
+}
