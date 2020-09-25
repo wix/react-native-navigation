2b06101c6ed2d99ef406fc2880f6c35f055c5436
Marc Rousavy <marcrousavy@hotmail.com>
2020-09-25 13:14:55 +0200

    Create interpolator files

---
+++ lib/ios/Interpolators/OvershootInterpolator.m
@@ -0,0 +1,9 @@
+//
+//  OvershootInterpolator.m
+//  ReactNativeNavigation
+//
+//  Created by Marc Rousavy on 25.09.20.
+//  Copyright © 2020 Wix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
