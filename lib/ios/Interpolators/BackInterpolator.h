6e2f888463109cefae747cd5b0ae857d38f542a4
Marc Rousavy <marcrousavy@hotmail.com>
2020-09-25 13:54:07 +0200

    Implement BackInterpolator

---
+++ lib/ios/Interpolators/BackInterpolator.h
@@ -0,0 +1,19 @@
+//
+//  BackInterpolator.h
+//  ReactNativeNavigation
+//
+//  Created by Marc Rousavy on 25.09.20.
+//  Copyright © 2020 Wix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Interpolator.h"
+
+@interface BackInterpolator : NSObject<Interpolator>
+
+@property (readonly) CGFloat from;
+@property (readonly) CGFloat to;
+
+- (instancetype)init:(CGFloat)from to:(CGFloat)to;
+
+@end
