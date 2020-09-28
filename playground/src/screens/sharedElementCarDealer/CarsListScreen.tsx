432d977b27370fea97bbb997971a41036d2a9129
Marc Rousavy <marcrousavy@hotmail.com>
2020-09-28 15:08:52 +0200

    Update SET Todos

--- playground/src/screens/sharedElementCarDealer/CarsListScreen.tsx
+++ playground/src/screens/sharedElementCarDealer/CarsListScreen.tsx
@@ -8,14 +8,10 @@ import { buildSharedElementAnimations } from './Constants';
 import CarCard from './CarCard';
 
 // SET = Shared Element Transition
-// TODO: 1. Animate borderRadius in SET: https://github.com/wix/react-native-navigation/issues/6519
-// TODO: 2. Spring interpolation including configuration of mass/springiness: https://github.com/wix/react-native-navigation/issues/6431
-// TODO: 3. Fix weird ScrollView insets on iOS in the PostDetailsScreen, see: https://github.com/wix/react-native-navigation/issues/6510
-// TODO: 4. Fix image transitions on iOS, see: https://github.com/wix/react-native-navigation/issues/6505
-// TODO: 5. Make SETs for Overlays possible OR allow parent screen to be visible beneath PostDetailsScreen to allow animation similar to Apple's App of the Day (AppStore) animation: https://github.com/wix/react-native-navigation/issues/6431
-// TODO: 6. Fix ScaleTypeCenterCrop hard-crash on Android: https://github.com/wix/react-native-navigation/issues/6517
-// TODO: 7. Add bottomTabs animation support so it slides out nicely (translateY): https://github.com/wix/react-native-navigation/issues/6340 and https://github.com/wix/react-native-navigation/issues/6567
-// TODO: 8. Add topBar animation support so it slides out nicely (translateY): (no issue for that yet?)
+// TODO: 1. Spring interpolation including configuration of mass/springiness: https://github.com/wix/react-native-navigation/issues/6431
+// TODO: 2. Make SETs for Overlays possible OR allow parent screen to be visible beneath PostDetailsScreen to allow animation similar to Apple's App of the Day (AppStore) animation: https://github.com/wix/react-native-navigation/issues/6431
+// TODO: 3. Add bottomTabs animation support so it slides out nicely (translateY): https://github.com/wix/react-native-navigation/issues/6340 and https://github.com/wix/react-native-navigation/issues/6567
+// TODO: 4. Add topBar animation support so it slides out nicely (translateY): (no issue for that yet?)
 
 const CarsListScreen: NavigationFunctionComponent = ({ componentId }) => {
   const onCarPressed = useCallback(
