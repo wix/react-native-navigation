1b702565ccde96d188c7e1b919011bacad23810c
Marc Rousavy <marcrousavy@hotmail.com>
2020-09-30 16:15:16 +0200

    Use Ease Out function for EaseInterpolator

--- lib/ios/Interpolators/EaseInterpolator.m
+++ lib/ios/Interpolators/EaseInterpolator.m
@@ -8,11 +8,6 @@
 
 #import "EaseInterpolator.h"
 
-static const CGFloat p0 = 0.42f;
-static const CGFloat p1 = 0;
-static const CGFloat p2 = 1;
-static const CGFloat p3 = 1;
-
 @implementation EaseInterpolator
 
 - (instancetype)init
@@ -22,17 +17,8 @@ static const CGFloat p3 = 1;
 }
 
 - (CGFloat)interpolate:(CGFloat)progress {
-	// ease = bezier(0.42, 0, 1, 1)
-	
-	if (progress < 0) {
-		return 0;
-	}
-	if (progress > 1) {
-		return 1;
-	}
-	
-	// https://en.wikipedia.org/wiki/Bézier_curve#Cubic_Bézier_curves
-	return powf(1 - progress, 3) * p0 + 3 * powf(1 - progress, 2) * progress * p1 + 3 * (1 - progress) * powf(progress, 2) * p2 + powf(progress, 3) * p3;
+	// https://easings.net/#easeOutCubic
+    return 1 - powf(1 - progress, 3);
 }
 
 @end
