e6fa5ab7b33a61c9ba639ae7cb9503b832fd5016
Danilo Bürger <info@danilobuerger.de>
2020-12-01 11:44:02 +0100

    Embed launch storyboard vc into RNNSplashScreen
    This will allow usage of preferredStatusBarStyle and prefersStatusBarHidden as defined in RNNSplashScreen

--- lib/ios/RNNSplashScreen.m
+++ lib/ios/RNNSplashScreen.m
@@ -16,7 +16,12 @@
         @try {
             UIStoryboard *storyboard = [UIStoryboard storyboardWithName:launchStoryBoard
                                                                  bundle:nil];
-            viewController = [storyboard instantiateInitialViewController];
+            UIViewController *launchVC = [storyboard instantiateInitialViewController];
+
+            viewController = [[RNNSplashScreen alloc] init];
+            [viewController addChildViewController:launchVC];
+            [viewController.view addSubview:launchVC.view];
+            [launchVC didMoveToParentViewController:viewController];
         } @catch (NSException *e) {
             UIView *splashView = [[NSBundle mainBundle] loadNibNamed:launchStoryBoard
                                                                owner:self
