bc55a88ccf708f5e962138a70bf4b360b6a0522b
rahulfancode <rahul@fancode.com>
2020-12-04 16:46:21 +0530

    fix(backbutton): back button option logic changes

--- lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/stack/topbar/button/BackButtonHelper.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/stack/topbar/button/BackButtonHelper.java
@@ -1,6 +1,5 @@
 package com.reactnativenavigation.viewcontrollers.stack.topbar.button;
 
-import com.reactnativenavigation.options.Options;
 import com.reactnativenavigation.options.params.Bool;
 import com.reactnativenavigation.viewcontrollers.viewcontroller.ViewController;
 
@@ -12,9 +11,8 @@ public class BackButtonHelper {
     }
 
     public void addToPushedChild(ViewController child) {
-        if (child.options.topBar.buttons.left != null || child.options.topBar.buttons.back.visible.isFalse()) return;
-        Options options = new Options();
-        options.topBar.buttons.back.setVisible();
-        child.mergeOptions(options);
+        if (child.options.topBar.buttons.left != null || child.options.topBar.buttons.back.visible.isFalse())
+            return;
+        child.options.topBar.buttons.back.setVisible();
     }
 }
