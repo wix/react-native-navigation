a77660d6cc7e73ff8244d93d6e228053a08b731a
Matei Oprea <eu@opreamatei.ro>
2020-12-05 15:41:49 +0200

    searchBar set focus
    Fixes #4290

--- playground/src/screens/OptionsScreen.tsx
+++ playground/src/screens/OptionsScreen.tsx
@@ -30,6 +30,10 @@ export default class Options extends React.Component<Props> {
         title: {
           text: 'Styling Options',
         },
+        searchBar: {
+          visible: true,
+          focus: true,
+        },
       },
     };
   }
