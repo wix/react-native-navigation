dfe21db5bff38e936f51c6912ae163c424555f16
yogevbd <yogev132@gmail.com>
2020-12-08 12:58:02 +0200

    Explicitly merge options to improve performance

--- lib/ios/RNNSearchBarOptions.m
+++ lib/ios/RNNSearchBarOptions.m
@@ -3,8 +3,7 @@
 @implementation RNNSearchBarOptions
 
 - (instancetype)initWithDict:(NSDictionary *)dict {
-    self = [super init];
-
+    self = [super initWithDict:dict];
     self.visible = [BoolParser parse:dict key:@"visible"];
     self.hideOnScroll = [BoolParser parse:dict key:@"hiddenWhenScrolling"];
     self.hideTopBarOnFocus = [BoolParser parse:dict key:@"hideTopBarOnFocus"];
@@ -14,6 +13,23 @@
     self.tintColor = [ColorParser parse:dict key:@"tintColor"];
     self.placeholder = [TextParser parse:dict key:@"placeholder"];
     return self;
+}
+
+- (void)mergeOptions:(RNNSearchBarOptions *)options {
+    if (options.visible.hasValue)
+        self.visible = options.visible;
+    if (options.hideOnScroll.hasValue)
+        self.hideOnScroll = options.hideOnScroll;
+    if (options.hideTopBarOnFocus.hasValue)
+        self.hideTopBarOnFocus = options.hideTopBarOnFocus;
+    if (options.obscuresBackgroundDuringPresentation.hasValue)
+        self.obscuresBackgroundDuringPresentation = options.obscuresBackgroundDuringPresentation;
+    if (options.backgroundColor.hasValue)
+        self.backgroundColor = options.backgroundColor;
+    if (options.tintColor.hasValue)
+        self.tintColor = options.tintColor;
+    if (options.placeholder.hasValue)
+        self.placeholder = options.placeholder;
 }
 
 @end
