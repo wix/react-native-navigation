a00605e5addb359a38e5084186497f8a6741a822
Ward Abbass <warda@wix.com>
2021-01-12 12:23:18 +0200

    Revert "Update BottomTabsPresenter.kt"
    This reverts commit fe488e85f296220dbe9cb5a234648a45d31ed9bd.

--- lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/bottomtabs/BottomTabsPresenter.kt
+++ lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/bottomtabs/BottomTabsPresenter.kt
@@ -9,7 +9,6 @@ import com.aurelhubert.ahbottomnavigation.AHBottomNavigation.TitleState
 import com.reactnativenavigation.options.Options
 import com.reactnativenavigation.viewcontrollers.viewcontroller.ViewController
 import com.reactnativenavigation.views.bottomtabs.BottomTabs
-import kotlin.math.max
 
 class BottomTabsPresenter(
         private val tabs: List<ViewController<*>>,
@@ -185,9 +184,5 @@ class BottomTabsPresenter(
                 appearingOptions.animations.setStackRoot.bottomTabs,
                 appearingOptions.bottomTabsOptions.visible
         )
-    }
-    fun findTabIndexByTabId(id: String?): Int {
-        val index = bottomTabFinder.findByControllerId(id)
-        return max(index, 0)
     }
 }
