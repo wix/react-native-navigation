dcb3c76f63cb3c566b0b5d056b5a964144daadc1
Ward Abbass <swabbass@gmail.com>
2021-02-17 16:41:00 +0200

    Update ModalPresenterTest.java
    reuse json from options test

--- lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/modal/ModalPresenterTest.java
+++ lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/modal/ModalPresenterTest.java
@@ -9,6 +9,7 @@ import com.reactnativenavigation.mocks.SimpleViewController;
 import com.reactnativenavigation.options.AnimationOptions;
 import com.reactnativenavigation.options.ModalAnimationOptions;
 import com.reactnativenavigation.options.ModalAnimationOptionsKt;
+import com.reactnativenavigation.options.ModalAnimationOptionsTestKt;
 import com.reactnativenavigation.options.ModalPresentationStyle;
 import com.reactnativenavigation.options.Options;
 import com.reactnativenavigation.options.params.Bool;
@@ -48,40 +49,6 @@ public class ModalPresenterTest extends BaseTest {
     private ViewController root;
     private CoordinatorLayout modalsLayout;
 
-    private JSONObject newBasicAnimation() {
-        JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("from", 0.0);
-            jsonObject.put("to", 1.0);
-            jsonObject.put("duration", 100);
-            JSONObject inter = new JSONObject();
-            inter.put("type", "decelerate");
-            jsonObject.put("interpolation", inter);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return jsonObject;
-    }
-
-    private JSONObject createModalAnimationOptions(Boolean enabled) {
-        JSONObject enter = new JSONObject();
-        JSONObject exit = new JSONObject();
-        JSONObject json = new JSONObject();
-
-        try {
-            enter.put("enabled", enabled);
-            enter.put("translationY", newBasicAnimation());
-
-            exit.put("enabled", enabled);
-            exit.put("translationY", newBasicAnimation());
-
-            json.put("enter", enter);
-            json.put("exit", exit);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return json;
-    }
 
     @Override
     public void beforeEach() {
@@ -139,9 +106,8 @@ public class ModalPresenterTest extends BaseTest {
     @Test
     public void showModal_resolvesDefaultOptions() throws JSONException {
         Options defaultOptions = new Options();
-        JSONObject disabledShowModalAnimation = createModalAnimationOptions(false);
+        JSONObject disabledShowModalAnimation = ModalAnimationOptionsTestKt.newModalAnimationJson(false);
         defaultOptions.animations.showModal = ModalAnimationOptionsKt.parseModalAnimationOptions(disabledShowModalAnimation);
-
         uut.setDefaultOptions(defaultOptions);
         uut.showModal(modal1, root, new CommandListenerAdapter());
         verifyZeroInteractions(animator);
