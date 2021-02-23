df0e136789d311d2bb136dbea6cb1487c2873380
yogevbd <yogev132@gmail.com>
2021-02-23 15:45:03 +0200

    Remove react log box from button and title components

--- lib/ios/RNNComponentView.m
+++ lib/ios/RNNComponentView.m
@@ -1,13 +1,47 @@
 #import "RNNComponentView.h"
 #import "RCTHelpers.h"
 
-@implementation RNNComponentView
+@implementation RNNComponentView {
+    BOOL _observeLayerChange;
+}
 
 - (void)layoutSubviews {
     [super layoutSubviews];
 #ifdef DEBUG
-    [RCTHelpers removeYellowBox:self];
+    [self removeYellowBox];
 #endif
 }
+
+#ifdef DEBUG
+- (void)removeYellowBox {
+    if (self.subviews.count > 0 && self.subviews.firstObject.subviews.count > 0) {
+        if (!_observeLayerChange) {
+            [self.subviews.firstObject.subviews.firstObject.layer
+                addObserver:self
+                 forKeyPath:@"sublayers"
+                    options:NSKeyValueObservingOptionNew
+                    context:nil];
+            _observeLayerChange = YES;
+        }
+
+        for (UIView *view in self.subviews.firstObject.subviews.firstObject.subviews) {
+            if ([view.accessibilityLabel isEqualToString:@"! Yellow Box"]) {
+                view.layer.opacity = 0;
+            }
+        }
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context {
+    if ([keyPath isEqualToString:@"sublayers"]) {
+        if (((CALayer *)object).sublayers.count > 1) {
+            ((CALayer *)object).sublayers.lastObject.opacity = 0;
+        }
+    }
+}
+#endif
 
 @end
