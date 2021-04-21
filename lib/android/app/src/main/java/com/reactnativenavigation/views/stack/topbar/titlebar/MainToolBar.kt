c86b095571508870978c2f2a7693330c62c12f60
Ward Abbass <swabbass@gmail.com>
2021-04-21 06:39:32 +0300

    phase 1
    - measure MainToolbar and set child measurements.
    - layout the title component according to the measurements.

--- lib/android/app/src/main/java/com/reactnativenavigation/views/stack/topbar/titlebar/MainToolBar.kt
+++ lib/android/app/src/main/java/com/reactnativenavigation/views/stack/topbar/titlebar/MainToolBar.kt
@@ -1,12 +1,12 @@
 package com.reactnativenavigation.views.stack.topbar.titlebar
 
 import android.content.Context
-import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
-import android.widget.FrameLayout
 import android.widget.RelativeLayout
 import androidx.annotation.ColorInt
 import androidx.annotation.RestrictTo
+import androidx.core.view.iterator
 import com.reactnativenavigation.options.Alignment
 import com.reactnativenavigation.options.FontOptions
 import com.reactnativenavigation.options.params.Colour
@@ -15,15 +15,25 @@ import com.reactnativenavigation.utils.CompatUtils
 import com.reactnativenavigation.utils.UiUtils
 import com.reactnativenavigation.utils.ViewUtils
 import com.reactnativenavigation.utils.logd
+import kotlin.math.max
+import kotlin.math.min
 
 const val DEFAULT_LEFT_MARGIN = 16
+const val SYSTEM_ACTION_BUTTON_SIZE_DP = 48
 
 class MainToolBar(context: Context) : RelativeLayout(context) {
 
     private var component: View? = null
-    private val centerFrameLayout = FrameLayout(context).apply {
-        id = CompatUtils.generateViewId()
-    }
+
+    private var titleComponentAlignment: Alignment = Alignment.Default
+        set(value) {
+            if (field != value) {
+                field = value
+                requestLayout()
+            }
+        }
+    private val defaultMargin = UiUtils.dpToPx(context, DEFAULT_LEFT_MARGIN)
+    private val minToolBarSize = UiUtils.dpToPx(context, SYSTEM_ACTION_BUTTON_SIZE_DP)
 
     private val titleSubTitleBar = TitleSubTitleLayout(context).apply {
         id = CompatUtils.generateViewId()
@@ -41,37 +51,20 @@ class MainToolBar(context: Context) : RelativeLayout(context) {
             addRule(ALIGN_PARENT_START)
             addRule(CENTER_VERTICAL)
         })
-        this.addView(titleSubTitleBar, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
-            addRule(END_OF, leftButtonsBar.id)
-            addRule(START_OF, rightButtonsBar.id)
-            addRule(CENTER_VERTICAL)
-            marginStart = UiUtils.dpToPx(context, DEFAULT_LEFT_MARGIN)
-        })
-        this.addView(centerFrameLayout, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT))
+        this.addView(titleSubTitleBar, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT))
         this.addView(rightButtonsBar, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
             addRule(ALIGN_PARENT_END)
             addRule(CENTER_VERTICAL)
         })
     }
 
-
     fun setComponent(component: View, alignment: Alignment = Alignment.Default) {
         logd("setComponent with this.component:$${this.component?.id ?: -999}, newComponent:${component.id}, alignment:${alignment}")
         if (this.component == component) return
         clear()
         this.component = component
-        if (alignment == Alignment.Center) {
-            this.centerFrameLayout.addView(this.component, FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT).apply {
-                this.gravity = Gravity.CENTER
-            })
-        } else {
-            this.addView(this.component, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
-                addRule(END_OF, leftButtonsBar.id)
-                addRule(START_OF, rightButtonsBar.id)
-                addRule(CENTER_VERTICAL)
-                marginStart = UiUtils.dpToPx(context, DEFAULT_LEFT_MARGIN)
-            })
-        }
+        this.addView(this.component,  LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT))
+        titleComponentAlignment = alignment
     }
 
     fun setTitle(title: CharSequence?) {
@@ -88,18 +81,7 @@ class MainToolBar(context: Context) : RelativeLayout(context) {
 
     fun setTitleBarAlignment(alignment: Alignment) {
         logd("setTitleBarAlignment $alignment on ${if (this.component == null) "titleSubTitle" else "component"} $id")
-        this.titleSubTitleBar.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
-            if (alignment != Alignment.Center) {
-                addRule(END_OF, leftButtonsBar.id)
-                addRule(START_OF, rightButtonsBar.id)
-                addRule(CENTER_VERTICAL)
-                marginStart = UiUtils.dpToPx(context, DEFAULT_LEFT_MARGIN)
-            } else {
-                marginStart = 0
-                addRule(CENTER_IN_PARENT, TRUE)
-            }
-        }
-
+        titleComponentAlignment = alignment
     }
 
     override fun setLayoutDirection(layoutDirection: Int) {
@@ -160,6 +142,69 @@ class MainToolBar(context: Context) : RelativeLayout(context) {
         clearComponent()
     }
 
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        super.onLayout(changed, l, t, r, b)
+        // Bypass regular layout for the titleComponent to make final placements based on the measurement
+        val titleComponent = getTitleComponent()
+        if (titleComponentAlignment == Alignment.Center) {
+            val titleLeft = max(l + leftButtonsBar.measuredWidth, (r - l) / 2 - titleComponent.measuredWidth / 2)
+            val titleRight = min(r - rightButtonsBar.measuredWidth, (r - l) / 2 + titleComponent.measuredWidth / 2)
+            titleComponent.layout(titleLeft, t, titleRight, b)
+        } else {
+            titleComponent.layout(l + leftButtonsBar.measuredWidth + defaultMargin, t, r - rightButtonsBar.measuredWidth, b)
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val mode = MeasureSpec.getMode(widthMeasureSpec)
+        val parentWidth = MeasureSpec.getSize(widthMeasureSpec)
+        val parentHeight = MeasureSpec.getSize(heightMeasureSpec)
+        logd("OnMeasure: parentWidth: $parentWidth, parentHeight: $parentHeight widthMeasureSpec:" +
+                " ${MeasureSpec.toString(widthMeasureSpec)}, heightMeasureSpec: ${MeasureSpec.toString(heightMeasureSpec)}", "MainToolBar")
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        // exact measurement of the parent
+        if (mode == MeasureSpec.EXACTLY) {
+            val titleComponent = getTitleComponent()
+            // measure all 3 child with AT_MOST to get what space they require
+            rightButtonsBar.measure(MeasureSpec.makeMeasureSpec(parentWidth, MeasureSpec.AT_MOST), heightMeasureSpec)
+            leftButtonsBar.measure(MeasureSpec.makeMeasureSpec(parentWidth, MeasureSpec.AT_MOST), heightMeasureSpec)
+            titleComponent.measure(MeasureSpec.makeMeasureSpec(parentWidth, MeasureSpec.AT_MOST), heightMeasureSpec)
+
+            val titleComponentWidth = titleComponent.measuredWidth
+
+            // hide buttons title bypass the right end, otherwise show
+            var leftBarWidth = leftButtonsBar.measuredWidth
+            val titleLeft = parentWidth / 2 - (titleComponentWidth / 2)
+            if (titleLeft < this.left + leftBarWidth + defaultMargin) {
+                leftBarWidth = minToolBarSize
+                for (item in leftButtonsBar.menu) {
+                    item.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+                }
+            }else{
+                for (item in leftButtonsBar.menu) {
+                    item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
+                }
+            }
+
+            // hide buttons title bypass the left end, otherwise show
+            var rightBarWidth = rightButtonsBar.measuredWidth
+            val titleRight = parentWidth / 2 + (titleComponentWidth / 2)
+            if (titleRight > this.right - rightBarWidth - defaultMargin) {
+                rightBarWidth = minToolBarSize
+                for (item in rightButtonsBar.menu) {
+                    item.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+                }
+            }else{
+                for (item in rightButtonsBar.menu) {
+                    item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
+                }
+            }
+
+            // calculate and measure the title component for the last time.
+            val measuredTitleWidth = min(parentWidth - rightBarWidth - leftBarWidth, titleComponentWidth)
+            titleComponent.measure(MeasureSpec.makeMeasureSpec(measuredTitleWidth, MeasureSpec.EXACTLY), heightMeasureSpec)
+        }
+    }
 
     private fun clearComponent() = this.component?.let { ViewUtils.removeFromParent(it); this.component = null; }
 
