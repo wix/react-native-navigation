db23d7a744f7aea21421a4a3da4daf8d8a933ecf
Ward Abbass <swabbass@gmail.com>
2021-04-21 06:24:28 +0300

    limits lines for 1

--- lib/android/app/src/main/java/com/reactnativenavigation/views/stack/topbar/titlebar/TitleSubTitleLayout.kt
+++ lib/android/app/src/main/java/com/reactnativenavigation/views/stack/topbar/titlebar/TitleSubTitleLayout.kt
@@ -1,7 +1,7 @@
 package com.reactnativenavigation.views.stack.topbar.titlebar
 
 import android.content.Context
-import android.graphics.Color
+import android.text.TextUtils
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
@@ -15,10 +15,19 @@ import com.reactnativenavigation.options.Alignment
 import com.reactnativenavigation.options.FontOptions
 import com.reactnativenavigation.options.parsers.TypefaceLoader
 
+
 class TitleSubTitleLayout(context: Context) : LinearLayout(context) {
 
-    private val titleTextView = TextView(context).apply { TextViewCompat.setTextAppearance(this, R.style.TitleBarTitle) }
-    private val subTitleTextView = TextView(context).apply { TextViewCompat.setTextAppearance(this, R.style.TitleBarSubtitle) }
+    private val titleTextView = TextView(context).apply {
+        maxLines = 1
+        ellipsize = TextUtils.TruncateAt.END
+        TextViewCompat.setTextAppearance(this, R.style.TitleBarTitle)
+    }
+    private val subTitleTextView = TextView(context).apply {
+        maxLines = 1
+        ellipsize = TextUtils.TruncateAt.END
+        TextViewCompat.setTextAppearance(this, R.style.TitleBarSubtitle)
+    }
 
     init {
         this.orientation = VERTICAL
@@ -44,6 +53,35 @@ class TitleSubTitleLayout(context: Context) : LinearLayout(context) {
         } else {
             (this.titleTextView.layoutParams as LayoutParams).gravity = Gravity.START or Gravity.CENTER_VERTICAL
         }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+//        if (titleTextView.text.isNotEmpty() || subTitleTextView.text.isNotEmpty()) {
+//            val titleTextWidth = this.titleTextView.paint.measureText(this.titleTextView.text.toString())
+//            val subtitleTextWidth = this.subTitleTextView.paint.measureText(this.subTitleTextView.text.toString())
+//            val mode = MeasureSpec.getMode(widthMeasureSpec)
+//            val parentWidth = MeasureSpec.getSize(widthMeasureSpec)
+//            val parentHeight = MeasureSpec.getSize(heightMeasureSpec)
+//            val textWidth = max(titleTextWidth, subtitleTextWidth)
+//            super.onMeasure(widthMeasureSpec, heightMeasureSpec) // must call to meet layout constraints if any
+//            logd("OnMeasure: parentHeight: $parentHeight widthMeasureSpec: ${MeasureSpec.toString(widthMeasureSpec)}, heightMeasureSpec: ${MeasureSpec.toString(heightMeasureSpec)}", "TitleSubTitleLayout")
+//            if (mode == MeasureSpec.AT_MOST) {
+//                //back + overflow limit
+//                val maxAllowedWidth = parentWidth
+//                val desiredWidth = min(textWidth, maxAllowedWidth)
+//
+//                logd("OnMeasure: parentWidth: $parentWidth, titleTextWidth: $titleTextWidth, subtitleTextWidth: $subtitleTextWidth, desiredWidth: $desiredWidth, availableWidth: $maxAllowedWidth", "TitleSubTitleLayout")
+//                super.setMeasuredDimension(desiredWidth.roundToInt(), parentHeight)
+//            } else if (mode == MeasureSpec.EXACTLY) {
+//                //should check available space
+//                super.setMeasuredDimension(textWidth.roundToInt(), parentHeight)
+//            }
+//        } else {
+//            super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+//        }
+
     }
 
     fun setTitleFontSize(size: Float) = titleTextView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, size)
