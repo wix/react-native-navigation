ee464a0eb7f17f173508a6251b30fdcc303e8a7c
Ward Abbass <swabbass@gmail.com>
2021-07-18 11:58:26 +0300

    unused imports

--- lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/bottomtabs/BottomTabsController.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/bottomtabs/BottomTabsController.java
@@ -2,7 +2,6 @@ package com.reactnativenavigation.viewcontrollers.bottomtabs;
 
 import android.animation.Animator;
 import android.app.Activity;
-import android.content.res.Configuration;
 import android.view.View;
 import android.view.ViewGroup;
 
--- lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/child/ChildController.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/child/ChildController.java
@@ -1,7 +1,6 @@
 package com.reactnativenavigation.viewcontrollers.child;
 
 import android.app.Activity;
-import android.content.res.Configuration;
 import android.view.View;
 import android.view.ViewGroup;
 
