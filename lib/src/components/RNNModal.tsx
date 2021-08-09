501a62cd4bbdbeaf6902dea502b3436e2db4bd61
Ward Abbass <swabbass@gmail.com>
2021-08-09 18:56:47 +0300

    modal component return null when not visible

--- lib/src/components/RNNModal.tsx
+++ lib/src/components/RNNModal.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { requireNativeComponent } from 'react-native';
+import { requireNativeComponent, ViewProps } from 'react-native';
+export interface RNNModalProps extends ViewProps {
+  animationType?: 'none' | 'slide' | 'fade' | null;
+  presentationStyle?: 'fullScreen' | 'pageSheet' | 'formSheet' | 'overFullScreen' | null;
+  transparent?: boolean;
+  statusBarTranslucent?: boolean;
+  hardwareAccelerated?: boolean;
+  visible?: boolean;
+}
 
 const RNNModalViewManager = requireNativeComponent('RNNModalViewManager');
 
-export class RNNModal extends React.Component {
+export class RNNModal extends React.Component<RNNModalProps> {
+  constructor(props: RNNModalProps) {
+    super(props);
+  }
   render() {
+    console.log(`xxxxxxx, visibility: ${this.props.visible}`);
+    if (this.props.visible !== true) return null;
     return <RNNModalViewManager {...this.props} />;
   }
 }
