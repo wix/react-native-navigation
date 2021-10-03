01fdfa4e11047348894ca88e450eb35b6958d994
Ward Abbass <swabbass@gmail.com>
2021-10-03 14:03:39 +0300

    revert local changes

--- scripts/start.js
+++ scripts/start.js
@@ -6,7 +6,7 @@ if (isWindows) runWin32();
 else run();
 
 function run() {
-  // exec.killPort(8081);
+  exec.killPort(8081);
   exec.execSync(`watchman watch-del-all || true`);
   exec.execSync(`adb reverse tcp:8081 tcp:8081 || true`);
   exec.execSync(`node ./node_modules/react-native/local-cli/cli.js start`);
