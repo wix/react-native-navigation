06ad071c71e40d833e49d3115258d1d15fb636af
Ward Abbass <swabbass@gmail.com>
2021-10-06 06:39:19 +0300

    Update ModalViewManager.kt

--- lib/android/app/src/main/java/com/reactnativenavigation/react/modal/ModalViewManager.kt
+++ lib/android/app/src/main/java/com/reactnativenavigation/react/modal/ModalViewManager.kt
@@ -33,7 +33,7 @@ class ModalViewManager(val reactContext: ReactContext) : ViewGroupManager<ModalH
         get() {
             val navigationActivity = reactContext.currentActivity as? NavigationActivity
             return navigationActivity?.let {
-                if (it.isFinishing || it.isDestroyed) null else navigator
+                if (it.isFinishing || it.isDestroyed) null else it.navigator
             }
         }
     private val jsonParser = JSONParser()
