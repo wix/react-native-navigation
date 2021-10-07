59496d331e7c379e6d75b92002c30375f2d0d956
Ward Abbass <swabbass@gmail.com>
2021-10-07 08:32:55 +0300

    upgrade reanimated

--- playground/android/app/src/main/java/com/reactnativenavigation/playground/MainApplication.java
+++ playground/android/app/src/main/java/com/reactnativenavigation/playground/MainApplication.java
@@ -1,11 +1,15 @@
 package com.reactnativenavigation.playground;
 
+import androidx.annotation.Nullable;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JSIModulePackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationPackage;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +18,11 @@ public class MainApplication extends NavigationApplication {
 
     private final ReactNativeHost mReactNativeHost =
             new NavigationReactNativeHost(this) {
+                @Override
+                protected JSIModulePackage getJSIModulePackage() {
+                    return new ReanimatedJSIModulePackage();
+                }
+
                 @Override
                 protected String getJSMainModuleName() {
                     return "index";
