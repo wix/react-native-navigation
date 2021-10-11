52a734565589f5b1f8248fbf07e5128298e016a2
Ward Abbass <swabbass@gmail.com>
2021-10-11 14:32:28 +0300

    RN66
    - Normal unit tests working
    - Upgrade to RN66.
    - Move jest to jest config file.
    - Use babel-test transformer to make unit tests working
    - e2e tests that can run as unit wont work.

---
+++ jest.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|react-native-ui-lib|@sentry/react-native)/)',
+  ],
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
+  roots: [
+    '<rootDir>/lib/src/',
+    '<rootDir>/playground/src/',
+    '<rootDir>/integration/',
+    '<rootDir>/scripts/',
+    '<rootDir>/e2e/',
+  ],
+  setupFilesAfterEnv: ['./jest-setup.js'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    'react-native-navigation/Mock': '<rootDir>/lib/src/Mock',
+    'react-native-navigation': '<rootDir>/lib/src',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/playground/img/layouts@2x.png',
+  },
+  collectCoverageFrom: [
+    'lib/src/**/*.ts',
+    'lib/src/**/*.tsx',
+    'integration/**/*.js',
+    '!lib/dist/index.js',
+    '!lib/dist/Navigation.js',
+    '!lib/dist/adapters/**/*',
+    '!lib/dist/interfaces/**/*',
+    '!lib/dist/**/*.test.*',
+    '!integration/**/*.test.*',
+    '!integration/*.test.*',
+    '!e2e/**/*test.js',
+  ],
+  resetMocks: true,
+  resetModules: true,
+  coverageReporters: ['json', 'lcov', 'text', 'html'],
+};
