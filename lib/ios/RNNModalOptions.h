cf73780dfd58e3946db96e1d51509dc316fb1417
svbutko <svbutko@hotmail.com>
2021-10-20 11:17:59 +0300

    Add RNNModalOptions

--- lib/ios/RNNModalOptions.h
+++ lib/ios/RNNModalOptions.h
@@ -3,5 +3,12 @@
 @interface RNNModalOptions : RNNOptions
 
 @property(nonatomic, strong) Bool *swipeToDismiss;
+@property(nonatomic, strong) Bool *prefersScrollingExpandsWhenScrolledToEdge;
+@property(nonatomic, strong) Bool *prefersEdgeAttachedInCompactHeight;
+@property(nonatomic, strong) Bool *widthFollowsPreferredContentSizeWhenEdgeAttached;
+@property(nonatomic, strong) Bool *prefersGrabberVisible;
+@property(nonatomic, strong) Number *preferredCornerRadius;
+@property(nonatomic, strong) NSArray<Text *> *detents;
+@property(nonatomic, strong) Text *largestUndimmedDetent;
 
 @end
--- lib/ios/RNNModalOptions.m
+++ lib/ios/RNNModalOptions.m
@@ -1,16 +1,41 @@
 #import "RNNModalOptions.h"
+#import "OptionsArrayParser.h"
 
 @implementation RNNModalOptions
 
 - (instancetype)initWithDict:(NSDictionary *)dict {
     self = [super initWithDict:dict];
     self.swipeToDismiss = [BoolParser parse:dict key:@"swipeToDismiss"];
+	self.prefersScrollingExpandsWhenScrolledToEdge = [BoolParser parse:dict key:@"prefersScrollingExpandsWhenScrolledToEdge"];
+	self.prefersEdgeAttachedInCompactHeight = [BoolParser parse:dict key:@"prefersEdgeAttachedInCompactHeight"];
+	self.widthFollowsPreferredContentSizeWhenEdgeAttached = [BoolParser parse:dict key:@"widthFollowsPreferredContentSizeWhenEdgeAttached"];
+	self.prefersGrabberVisible = [BoolParser parse:dict key:@"prefersGrabberVisible"];
+	self.preferredCornerRadius = [NumberParser parse:dict key:@"preferredCornerRadius"];
+	self.largestUndimmedDetent = [TextParser parse:dict key:@"largestUndimmedDetent"];
+	self.detents = [OptionsArrayParser parse:dict key:@"detents" ofClass:Text.class];
     return self;
 }
 
 - (void)mergeOptions:(RNNModalOptions *)options {
     if (options.swipeToDismiss.hasValue)
         self.swipeToDismiss = options.swipeToDismiss;
+	if (options.prefersScrollingExpandsWhenScrolledToEdge.hasValue) {
+		self.prefersScrollingExpandsWhenScrolledToEdge = options.prefersScrollingExpandsWhenScrolledToEdge;
+	}
+	if (options.prefersEdgeAttachedInCompactHeight.hasValue)
+		self.prefersEdgeAttachedInCompactHeight = options.prefersEdgeAttachedInCompactHeight;
+	if (options.widthFollowsPreferredContentSizeWhenEdgeAttached.hasValue) {
+		self.widthFollowsPreferredContentSizeWhenEdgeAttached = options.widthFollowsPreferredContentSizeWhenEdgeAttached;
+	}
+	if (options.prefersGrabberVisible.hasValue)
+		self.prefersGrabberVisible = options.prefersGrabberVisible;
+	if (options.preferredCornerRadius.hasValue)
+		self.preferredCornerRadius = options.preferredCornerRadius;
+	if (options.largestUndimmedDetent.hasValue)
+		self.largestUndimmedDetent = options.largestUndimmedDetent;
+	if (options.detents)
+		self.detents = options.detents;
+	
 }
 
 @end
