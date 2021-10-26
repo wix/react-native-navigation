fce7eef68bf77efe3b1bbe52af2c41720cfc3c42
Ward Abbass <swabbass@gmail.com>
2021-10-26 15:28:14 +0300

    ad keyboard screen button to layouts

--- playground/src/screens/Screens.ts
+++ playground/src/screens/Screens.ts
@@ -120,6 +120,7 @@ const Screens = {
   },
   StatusBarOptions,
   StatusBarFirstTab,
+  KeyboardScreen: 'KeyboardScreens',
   TopBarBackground: 'TopBarBackground',
   Toast: 'Toast',
   FlatListScreen: 'FlatListScreen',
