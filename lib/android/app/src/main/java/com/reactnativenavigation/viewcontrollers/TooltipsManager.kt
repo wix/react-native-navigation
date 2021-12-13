4e516d0031fe8ad08e611d5d0a9d7e578db6c237
Ward Abbass <swabbass@gmail.com>
2021-12-13 18:52:25 +0200

    Move tooltips management to TooltipsManager

--- lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/TooltipsManager.kt
+++ lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/TooltipsManager.kt
@@ -1,15 +1,62 @@
 package com.reactnativenavigation.viewcontrollers
 
+import android.view.View
+import com.reactnativenavigation.options.OverlayAttachOptions
+import com.reactnativenavigation.react.CommandListener
 import com.reactnativenavigation.viewcontrollers.viewcontroller.ViewController
+import com.reactnativenavigation.views.MaViewTooltip
 
-class TooltipsManager {
-    private val registry = mutableMapOf<String, ViewController<*>>()
+class TooltipsManager(
+    val findController: (String) -> ViewController<*>?,
+    val findTooltipAnchorView: (OverlayAttachOptions) -> View?
+) {
+    private val registry = mutableMapOf<String, Pair<MaViewTooltip.TooltipView, ViewController<*>>>()
 
-    fun add(id:String, controller: ViewController<*>){
-        registry[id] = controller
+    fun showTooltip(
+        tooltipController: ViewController<*>, overlayAttachOptions: OverlayAttachOptions,
+        listener: CommandListener
+    ) {
+        val hostController: ViewController<*>? = findController(overlayAttachOptions.layoutId.get())
+        if (hostController != null) {
+            val tooltipAnchorView: View? = findTooltipAnchorView(overlayAttachOptions)
+            if (tooltipAnchorView != null) {
+                val tooltipView = hostController.showTooltip(tooltipAnchorView, overlayAttachOptions, tooltipController)
+                registry[tooltipController.id] = tooltipView to tooltipController
+                listener.onSuccess(tooltipController.id)
+            } else {
+                listener.onError("Cannot find anchor view with id" + overlayAttachOptions.anchorId)
+            }
+        } else {
+            listener.onError("Cannot find layout with id" + overlayAttachOptions.layoutId)
+        }
     }
 
-    fun remove(id:String):ViewController<*>?{
-        return registry[id]
+    fun contains(id:String): Boolean {
+        return registry.containsKey(id)
     }
+    fun dismissTooltip(id: String, listener: CommandListener) {
+        registry.remove(id)?.let {
+            val (tpView, controller) = it
+            tpView.close()
+            tpView.post {
+                controller.destroy()
+                listener.onSuccess(id)
+            }
+        }?:listener.onError("Can't dismiss non-shown tooltip of id: $id")
+    }
+
+    fun dismissAllTooltip(listener: CommandListener) {
+        while (registry.isNotEmpty()) {
+            val first = registry.entries.first()
+            val (tpView, controller) = first.value
+            tpView.close()
+            tpView.post {
+                controller.destroy()
+            }
+            registry.remove(first.key)
+        }
+        listener.onSuccess("")
+
+    }
+
 }
