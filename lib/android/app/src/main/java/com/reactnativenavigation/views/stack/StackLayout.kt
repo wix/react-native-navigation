61939f3567d3a8d95bd1e3759c319ad101fb4481
Ward Abbass <swabbass@gmail.com>
2021-12-14 11:11:04 +0200

    stack layout to kotlin

---
+++ lib/android/app/src/main/java/com/reactnativenavigation/views/stack/StackLayout.kt
@@ -0,0 +1,50 @@
+package com.reactnativenavigation.views.stack
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import android.view.View
+import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.reactnativenavigation.utils.UiUtils
+import com.reactnativenavigation.viewcontrollers.stack.topbar.TopBarController
+import com.reactnativenavigation.views.component.Component
+import com.reactnativenavigation.views.component.Renderable
+import com.reactnativenavigation.views.stack.topbar.ScrollDIsabledBehavior
+import com.reactnativenavigation.views.tooltips.TooltipsOverlay
+
+@SuppressLint("ViewConstructor")
+class StackLayout(
+    context: Context,
+    topBarController: TopBarController,
+    val stackId: String
+) : CoordinatorLayout(context), Component {
+     val overlay: TooltipsOverlay = TooltipsOverlay(context, "Stack Layout")
+
+    init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            this.overlay.setBackgroundColor(Color.argb(0.5f,0f,1f,0f));
+        }
+        createLayout(topBarController)
+    }
+
+    private fun createLayout(topBarController: TopBarController) {
+        val topBar: View = topBarController.createView(context, this)
+        val lp = LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, UiUtils.getTopBarHeight(
+                context
+            )
+        )
+        lp.behavior = ScrollDIsabledBehavior()
+        addView(topBar, lp)
+        addView(overlay, LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+    }
+
+    override fun isRendered(): Boolean {
+        return childCount >= 2 &&
+                getChildAt(1) is Renderable &&
+                (getChildAt(1) as Renderable).isRendered
+    }
+
+
+}
