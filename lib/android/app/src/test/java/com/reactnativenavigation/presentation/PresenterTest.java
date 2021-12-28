e62a7ec34362ca06e592f9a8b70abcb05af07e44
Ward Abbass <swabbass@gmail.com>
2021-12-28 10:55:53 +0200

    unit test margins

--- lib/android/app/src/test/java/com/reactnativenavigation/presentation/PresenterTest.java
+++ lib/android/app/src/test/java/com/reactnativenavigation/presentation/PresenterTest.java
@@ -7,6 +7,7 @@ import android.widget.FrameLayout;
 
 import com.reactnativenavigation.BaseTest;
 import com.reactnativenavigation.options.Options;
+import com.reactnativenavigation.options.layout.Margins;
 import com.reactnativenavigation.options.params.Bool;
 import com.reactnativenavigation.utils.SystemUiUtils;
 import com.reactnativenavigation.viewcontrollers.viewcontroller.Presenter;
@@ -15,6 +16,7 @@ import com.reactnativenavigation.viewcontrollers.viewcontroller.ViewController;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
@@ -52,5 +54,22 @@ public class PresenterTest extends BaseTest {
                     ()-> SystemUiUtils.showStatusBar(any(),eq(spy)),times(1));
         });
 
+    }
+
+    @Test
+    public void applyOptions_shouldApplyMarginsToLayout(){
+        ViewGroup spy = Mockito.spy(new FrameLayout(activity));
+        spy.setLayoutParams(new ViewGroup.MarginLayoutParams(0,0));
+        Mockito.when(controller.getView()).thenReturn(spy);
+        final Options options = Options.EMPTY.copy();
+        options.layout.setMargins(new Margins(1,2,3,4));
+        Mockito.when(controller.resolveCurrentOptions()).thenReturn(options);
+
+        uut.applyOptions(controller,options);
+        ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) spy.getLayoutParams();
+        assertThat(mlp.topMargin).isEqualTo(1);
+        assertThat(mlp.leftMargin).isEqualTo(2);
+        assertThat(mlp.bottomMargin).isEqualTo(3);
+        assertThat(mlp.rightMargin).isEqualTo(4);
     }
 }
