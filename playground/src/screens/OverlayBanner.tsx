fbedc4605a4718d1b28f48a5f4b00bed3550e9bf
Ward Abbass <swabbass@gmail.com>
2021-12-29 19:35:46 +0200

    add adjust resize toggle

--- playground/src/screens/OverlayBanner.tsx
+++ playground/src/screens/OverlayBanner.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, ViewStyle, TextStyle, SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet, TextStyle, View, ViewStyle } from 'react-native';
 import { NavigationComponent, NavigationComponentProps, Options } from 'react-native-navigation';
 import testIDs from '../testIDs';
+import Button from '../components/Button';
+import Navigation from '../services/Navigation';
 
 interface Props extends NavigationComponentProps {
   incrementDismissedOverlays: any;
 }
 
 const { BANNER_OVERLAY } = testIDs;
-
+let adjustResize = true;
 export default class OverlayBanner extends NavigationComponent<Props> {
   static options(): Options {
     return {
       layout: {
-        adjustResize: false,
+        adjustResize: adjustResize,
       },
     };
   }
@@ -28,11 +30,26 @@ export default class OverlayBanner extends NavigationComponent<Props> {
         pointerEvents="box-none"
       >
         <View testID={BANNER_OVERLAY} style={styles.banner}>
-          <Text style={styles.text}>This is a floating banner</Text>
+          <Button
+            // @ts-ignore
+            size={'small'}
+            style={styles.text}
+            label="Toggle adjustResize Overlay"
+            onPress={this.toggleAdjustResize}
+          />
         </View>
       </SafeAreaView>
     );
   }
+
+  toggleAdjustResize = () => {
+    adjustResize = !adjustResize;
+    Navigation.mergeOptions(this.props.componentId, {
+      layout: {
+        adjustResize,
+      },
+    });
+  };
 }
 
 type Style = {
