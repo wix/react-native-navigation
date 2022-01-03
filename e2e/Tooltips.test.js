c0501ec96fba51c928bf57c1cb8c976618d2ec90
Ward Abbass <swabbass@gmail.com>
2022-01-03 16:45:46 +0200

    Update Tooltips.test.js

--- e2e/Tooltips.test.js
+++ e2e/Tooltips.test.js
@@ -3,77 +3,79 @@ import TestIDs from '../playground/src/testIDs';
 import Android from './AndroidUtils';
 
 const { elementByLabel, elementById, sleep } = Utils;
+describe('Tooltips',()=>{
+    describe(':android: Tooltips', () => {
+        beforeEach(async () => {
+            await device.launchApp({ newInstance: true });
+            await elementById(TestIDs.TOOLTIPS_SCREEN_BTN).tap();
+        });
 
-describe(':android: Tooltips', () => {
-  beforeEach(async () => {
-    await device.launchApp({ newInstance: true });
-    await elementById(TestIDs.TOOLTIPS_SCREEN_BTN).tap();
-  });
+        it.e2e('should show and hide tooltips', async () => {
+            await elementById(TestIDs.SHOW_TOOLTIP_MAIN_BTMTABS_TPBAR_HIT).tap();
+            await elementById(TestIDs.OK_BUTTON).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
+        });
 
-    it.e2e('should show and hide tooltips', async () => {
-        await elementById(TestIDs.SHOW_TOOLTIP_MAIN_BTMTABS_TPBAR_HIT).tap();
-        await elementById(TestIDs.OK_BUTTON).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
+        it.e2e('should be always in main bottom tabs layer', async () => {
+            await elementById(TestIDs.SHOW_TOOLTIP_MAIN_BTMTABS_TPBAR_HIT).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+            await elementById(TestIDs.BACK_BUTTON).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+            await elementById(TestIDs.TOOLTIPS_SCREEN_BTN).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+        });
+
+
+        it.e2e('should be hidden, but still in main bottom tabs layer when modal opened', async () => {
+            await elementById(TestIDs.SHOW_TOOLTIP_MAIN_BTMTABS_TPBAR_HIT).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+            await elementById(TestIDs.MODAL_BTN).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
+            await elementById(TestIDs.DISMISS_MODAL_TOPBAR_BTN).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+        });
+
+        it.e2e('should attach tooltip to stack', async () => {
+            await elementById(TestIDs.SHOW_TOOLTIP_LAYOUT_STACK_TPBAR_HIT).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+            await elementById(TestIDs.OPTIONS_TAB).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
+            await elementById(TestIDs.LAYOUTS_TAB).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+            await elementById(TestIDs.PUSH_BTN).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+        });
+
+        it.e2e('should keep tooltip on stack when pushing screen with same button id', async () => {
+            await elementById(TestIDs.SHOW_TOOLTIP_LAYOUT_STACK_TPBAR_HIT).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+            await elementById(TestIDs.PUSH_BTN).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+        });
+
+        it.e2e('should be hidden, but still in a stack layer when modal opened', async () => {
+            await elementById(TestIDs.SHOW_TOOLTIP_LAYOUT_STACK_TPBAR_HIT).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+            await elementById(TestIDs.MODAL_BTN).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
+            await elementById(TestIDs.DISMISS_MODAL_TOPBAR_BTN).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+        });
+
+
+        it.e2e('should attach tooltip to component', async () => {
+            await elementById(TestIDs.SHOW_TOOLTIP_COMPONENT_TPBAR_HIT).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+            await elementById(TestIDs.PUSH_BTN).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
+            await elementById(TestIDs.BACK_BUTTON).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+            await elementById(TestIDs.PUSH_PUSHED_SCREEN).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
+            await elementById(TestIDs.BACK_BUTTON).tap();
+            await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
+        });
+
     });
-
-  it.e2e('should be always in main bottom tabs layer', async () => {
-    await elementById(TestIDs.SHOW_TOOLTIP_MAIN_BTMTABS_TPBAR_HIT).tap();
-      await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-      await elementById(TestIDs.BACK_BUTTON).tap();
-      await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-      await elementById(TestIDs.TOOLTIPS_SCREEN_BTN).tap();
-      await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-  });
-
-
-    it.e2e('should be hidden, but still in main bottom tabs layer when modal opened', async () => {
-        await elementById(TestIDs.SHOW_TOOLTIP_MAIN_BTMTABS_TPBAR_HIT).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-        await elementById(TestIDs.MODAL_BTN).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
-        await elementById(TestIDs.DISMISS_MODAL_TOPBAR_BTN).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-    });
-
-    it.e2e('should attach tooltip to stack', async () => {
-        await elementById(TestIDs.SHOW_TOOLTIP_LAYOUT_STACK_TPBAR_HIT).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-        await elementById(TestIDs.OPTIONS_TAB).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
-        await elementById(TestIDs.LAYOUTS_TAB).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-        await elementById(TestIDs.PUSH_BTN).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-    });
-
-    it.e2e('should keep tooltip on stack when pushing screen with same button id', async () => {
-        await elementById(TestIDs.SHOW_TOOLTIP_LAYOUT_STACK_TPBAR_HIT).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-        await elementById(TestIDs.PUSH_BTN).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-    });
-
-    it.e2e('should be hidden, but still in a stack layer when modal opened', async () => {
-        await elementById(TestIDs.SHOW_TOOLTIP_LAYOUT_STACK_TPBAR_HIT).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-        await elementById(TestIDs.MODAL_BTN).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
-        await elementById(TestIDs.DISMISS_MODAL_TOPBAR_BTN).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-    });
-
-
-    it.e2e('should attach tooltip to component', async () => {
-        await elementById(TestIDs.SHOW_TOOLTIP_COMPONENT_TPBAR_HIT).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-        await elementById(TestIDs.PUSH_BTN).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
-        await elementById(TestIDs.BACK_BUTTON).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-        await elementById(TestIDs.PUSH_PUSHED_SCREEN).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeNotVisible();
-        await elementById(TestIDs.BACK_BUTTON).tap();
-        await expect(elementById(TestIDs.OK_BUTTON)).toBeVisible();
-    });
-
 });
+
