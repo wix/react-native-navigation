wix/react-native-navigation

commit a867b417926e51a56f289da92111c32c798d3f05

Bottom tabs shadow default visibility (#6958)
* update padding top to 0 when shadow is not shown

* Update ShadowLayout.kt

* Update lib/android/app/src/main/java/com/reactnativenavigation/views/bottomtabs/ShadowLayout.kt

Co-authored-by: Yogev Ben David <yogev132@gmail.com>

lib/android/app/src/main/java/com/reactnativenavigation/views/bottomtabs/BottomTabsContainer.kt
@@ -35,6 +35,7 @@ class BottomTabsContainer(context: Context, val bottomTabs: BottomTabs) : Shadow
         }
 
     init {
+        this.isShadowed = false
         this.id = View.generateViewId()
         shadowAngle = DEFAULT_SHADOW_ANGLE
         shadowDistance = DEFAULT_SHADOW_DISTANCE

lib/android/app/src/main/java/com/reactnativenavigation/views/bottomtabs/ShadowLayout.kt
@@ -43,6 +43,7 @@ open class ShadowLayout constructor(context: Context) : FrameLayout(context) {
     var isShadowed: Boolean = false
         set(isShadowed) {
             field = isShadowed
+            this.updatePadding()
             postInvalidate()
         }
 
@@ -75,9 +76,13 @@ open class ShadowLayout constructor(context: Context) : FrameLayout(context) {
     private fun resetShadow() {
         shadowDx = (shadowDistance * cos(shadowAngle / 180.0f * Math.PI)).toFloat()
         shadowDy = (shadowDistance * sin(shadowAngle / 180.0f * Math.PI)).toFloat()
-        val padding = (shadowDistance + shadowRadius).toInt()
-        setPadding(0, padding, 0, 0)
+        updatePadding()
         requestLayout()
+    }
+
+    private fun updatePadding() {
+        val padding = if (isShadowed) (shadowDistance + shadowRadius).toInt() else 0
+        setPadding(0, padding, 0, 0)
     }
 
     private fun adjustShadowAlpha(adjust: Boolean): Int {

commit a4c9534474d8ae0d7ad9ee85dd36e94c699512bd

Fix elevation not visible on bottomTabs
Apparently elevation doesn't work if the view it's applied on doesn't have a background color. Regression introduced in #6958

Fixes #7422

lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/bottomtabs/BottomTabsPresenter.kt
@@ -71,7 +71,7 @@ class BottomTabsPresenter(
             bottomTabs.titleState = bottomTabsOptions.titleDisplayMode.toState()
         }
         if (bottomTabsOptions.backgroundColor.hasValue()) {
-            bottomTabs.setBackgroundColor(bottomTabsOptions.backgroundColor.get())
+            bottomTabsContainer.setBackgroundColor(bottomTabsOptions.backgroundColor.get())
         }
         if (bottomTabsOptions.animateTabSelection.hasValue()) {
             bottomTabs.setAnimateTabSelection(bottomTabsOptions.animateTabSelection.get())
@@ -143,7 +143,7 @@ class BottomTabsPresenter(
         bottomTabs.setLayoutDirection(options.layout.direction)
         bottomTabs.setPreferLargeIcons(options.bottomTabsOptions.preferLargeIcons[false])
         bottomTabs.titleState = bottomTabsOptions.titleDisplayMode[defaultTitleState]
-        bottomTabs.setBackgroundColor(bottomTabsOptions.backgroundColor.get(Color.WHITE)!!)
+        bottomTabsContainer.setBackgroundColor(bottomTabsOptions.backgroundColor.get(Color.WHITE)!!)
         bottomTabs.setAnimateTabSelection(bottomTabsOptions.animateTabSelection.get(true))
         if (bottomTabsOptions.currentTabIndex.hasValue()) {
             val tabIndex = bottomTabsOptions.currentTabIndex.get()
@@ -175,7 +175,7 @@ class BottomTabsPresenter(
             }
         }
         if (bottomTabsOptions.elevation.hasValue()) {
-            bottomTabs.setUseElevation(true, bottomTabsOptions.elevation.get().toFloat())
+            bottomTabsContainer.setElevation(bottomTabsOptions.elevation)
         }
 
         when {

lib/android/app/src/main/java/com/reactnativenavigation/views/bottomtabs/BottomTabsContainer.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.widget.LinearLayout
 import androidx.annotation.RestrictTo
 import androidx.core.graphics.ColorUtils
+import com.reactnativenavigation.options.params.Fraction
+import com.reactnativenavigation.utils.UiUtils.dpToPx
 import kotlin.math.roundToInt
 
 
@@ -90,6 +92,10 @@ class BottomTabsContainer(context: Context, val bottomTabs: BottomTabs) : Shadow
 
     fun hideTopOutLine() {
         topOutLineView.alpha = 0f
+    }
+
+    fun setElevation(elevation: Fraction) {
+        setElevation(dpToPx(context, elevation.get().toFloat()))
     }
 }
 

playground/src/commons/options/Options.ts
@@ -27,7 +27,8 @@ const setDefaultOptions = () =>
     },
     bottomTabs: {
       backgroundColor: Colors.barBackground,
-      tabsAttachMode: 'onSwitchToTab',
+      tabsAttachMode: 'afterInitialTab',
+      elevation: 8,
       titleDisplayMode: 'alwaysShow',
     },
     bottomTab: {

lib/android/app/src/test/java/com/reactnativenavigation/viewcontrollers/bottomtabs/BottomTabsPresenterTest.kt
@@ -68,7 +68,7 @@ class BottomTabsPresenterTest : BaseTest() {
         val options = Options()
         options.bottomTabsOptions.backgroundColor = ThemeColour(Colour(10))
         uut.mergeChildOptions(options, tabs[0])
-        verify(bottomTabs).setBackgroundColor(options.bottomTabsOptions.backgroundColor.get())
+        verify(bottomTabsContainer).setBackgroundColor(options.bottomTabsOptions.backgroundColor.get())
         verifyNoMoreInteractions(bottomTabs)
     }
 