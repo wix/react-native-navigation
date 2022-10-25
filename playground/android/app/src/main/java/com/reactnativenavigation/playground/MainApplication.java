3bb1af5fa04a28e8e4d8fb91c171a3344efaf799
Yogev Ben David <yogev132@gmail.com>
2022-10-25 12:22:43 +0300

    Fix android with old arch

--- playground/android/app/src/main/java/com/reactnativenavigation/playground/MainApplication.java
+++ playground/android/app/src/main/java/com/reactnativenavigation/playground/MainApplication.java
@@ -6,8 +6,6 @@ import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationPackage;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
-import com.facebook.react.bridge.JSIModulePackage;
 import com.reactnativenavigation.playground.newarchitecture.MainApplicationReactNativeHost;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +14,6 @@ public class MainApplication extends NavigationApplication {
 
     private final ReactNativeHost mReactNativeHost =
             new NavigationReactNativeHost(this) {
-                @Override
-                protected JSIModulePackage getJSIModulePackage() {
-                    return new ReanimatedJSIModulePackage();
-                }
                 @Override
                 protected String getJSMainModuleName() {
                     return "index";
