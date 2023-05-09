fed3f37a77de0e6555569752111d2a16d49d10b7
Evgeni Silman <evgenis@wix.com>
2023-05-09 12:13:37 +0300

    A fix (possible) of title bar width issue.

--- lib/android/app/src/reactNative71/java/com/reactnativenavigation/views/stack/topbar/titlebar/TitleBarReactView.kt
+++ lib/android/app/src/reactNative71/java/com/reactnativenavigation/views/stack/topbar/titlebar/TitleBarReactView.kt
@@ -13,47 +13,39 @@ import com.reactnativenavigation.react.ReactView
 class TitleBarReactView(context: Context?, reactInstanceManager: ReactInstanceManager?, componentId: String?,
                         componentName: String?) : ReactView(context, reactInstanceManager, componentId, componentName) {
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(interceptReactRootViewMeasureSpecWidth(widthMeasureSpec), interceptReactRootViewMeasureSpecHeight(heightMeasureSpec))
+        super.onMeasure(interceptReactRootViewMeasureSpec(widthMeasureSpec), heightMeasureSpec)
     }
 
-    private fun interceptReactRootViewMeasureSpecWidth(widthMeasureSpec: Int): Int {
+    private fun interceptReactRootViewMeasureSpec(widthMeasureSpec: Int): Int {
         // This is a HACK.
         // ReactRootView has problematic behavior when setting width to WRAP_CONTENT,
         // It's causing infinite measurements, that hung up the UI.
         // Intercepting largest child by width, and use its width as (parent) ReactRootView width fixed that.
         // See for more details https://github.com/wix/react-native-navigation/pull/7096
-        val measuredWidth = this.getLastRootViewChild()?.width
-
-        return if (measuredWidth != null) MeasureSpec.makeMeasureSpec(measuredWidth, MeasureSpec.EXACTLY) else
+        val view = getWidestView(rootViewGroup, 0)
+        val measuredWidth = view?.measuredWidth ?: 0
+        return if (measuredWidth > 0) MeasureSpec.makeMeasureSpec(measuredWidth, MeasureSpec.EXACTLY) else
             widthMeasureSpec
     }
 
-    private fun interceptReactRootViewMeasureSpecHeight(heightMeasureSpec: Int): Int {
-        // This is a HACK.
-        // ReactRootView has problematic behavior when setting width to WRAP_CONTENT,
-        // It's causing infinite measurements, that hung up the UI.
-        // Intercepting largest child by height, and use its height as (parent) ReactRootView width fixed that.
-        // See for more details https://github.com/wix/react-native-navigation/pull/7096
-        val measuredHeight = this.getLastRootViewChild()?.height
-
-        return if (measuredHeight != null) MeasureSpec.makeMeasureSpec(measuredHeight, MeasureSpec.EXACTLY) else
-            heightMeasureSpec
-    }
-
-    private fun getLastRootViewChild(): View? {
-        if (rootViewGroup.children.count() == 0) {
+    private fun getWidestView(viewGroup: ViewGroup, currentMaxWidth: Int): View? {
+        if (viewGroup.children.count() == 0) {
             return null
         }
-        var rootViewGroupLastChild: View = rootViewGroup
-        var next = rootViewGroup as Any
-        while(next is ViewGroup && next.childCount > 0) try {
-            rootViewGroupLastChild = next
-            next.children.first().also { next = it }
 
-        } catch (e: Exception) {
-            Log.i("TitleBarReactView", "getRootViewFirstChild: ${e.message}")
+        var maxWidth = currentMaxWidth
+        var result: View? = null
+
+        for (child in viewGroup.children) {
+            if (child is ViewGroup) {
+                result = getWidestView(child, maxWidth)
+                maxWidth = result?.measuredWidth ?: 0
+            } else if (child.measuredWidth > maxWidth){
+                maxWidth = child.measuredWidth
+                result = child
+            }
         }
-        @Suppress("UNREACHABLE_CODE")
-        return rootViewGroupLastChild
+
+        return result
     }
 }
