01e6a45b5b284405d3e010ebc1ac95fbfb6fd5ea
Doron Pearl <doronpr@users.noreply.github.com>
2023-05-22 14:20:31 +0300

    surface layout render errors (#7713)

--- lib/Mock/Components/LayoutComponent.tsx
+++ lib/Mock/Components/LayoutComponent.tsx
@@ -19,10 +19,8 @@ export const LayoutComponent = class extends Component<ComponentProps> {
     return <View />;
   }
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    console.error(
-      `Error while trying to render layout ${this.props.layoutNode.nodeId} of type ${this.props.layoutNode.type}`,
-      error,
-      errorInfo
+    throw new Error(
+      `Error while trying to render layout ${this.props.layoutNode.nodeId} of type ${this.props.layoutNode.type}: ${error}\n${errorInfo?.componentStack}`
     );
   }
 };
