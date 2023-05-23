630f426aecde1ae932ecf0534cf4942001755ab7
Ioannis Kokkinidis <sudoplz@gmail.com>
2023-05-23 17:34:28 +0300

    Adds support for fabric android overrides

--- playground/android/app/src/main/java/com/reactnativenavigation/playground/MainActivity.java
+++ playground/android/app/src/main/java/com/reactnativenavigation/playground/MainActivity.java
@@ -3,15 +3,19 @@ package com.reactnativenavigation.playground;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 import com.reactnativenavigation.NavigationActivity;
 
 import androidx.annotation.Nullable;
 
 public class MainActivity extends NavigationActivity {
+
+    @Override
+    protected String getMainComponentName() {
+        return "PlaygroundApp";
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,5 +27,22 @@ public class MainActivity extends NavigationActivity {
         ImageView img = new ImageView(this);
         img.setImageDrawable(getDrawable(R.drawable.ic_android));
         setContentView(img);
+    }
+
+    /**
+     * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+     * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+     * (aka React 18) with two boolean flags.
+     */
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new DefaultReactActivityDelegate(
+                this,
+                getMainComponentName(),
+                // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+                DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
+                // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+                DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
+        );
     }
 }
--- playground/android/app/src/main/java/com/reactnativenavigation/playground/MainApplication.java
+++ playground/android/app/src/main/java/com/reactnativenavigation/playground/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativenavigation.playground;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationPackage;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
@@ -30,6 +31,15 @@ public class MainApplication extends NavigationApplication {
                     packages.add(new NavigationPackage(mReactNativeHost));
                     return packages;
                 }
+
+                @Override
+                protected boolean isNewArchEnabled() {
+                    return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+                }
+                @Override
+                protected Boolean isHermesEnabled() {
+                    return BuildConfig.IS_HERMES_ENABLED;
+                }
             };
     private final ReactNativeHost mNewArchitectureNativeHost =
             new MainApplicationReactNativeHost(this);
@@ -38,6 +48,11 @@ public class MainApplication extends NavigationApplication {
     public void onCreate() {
         super.onCreate();
         registerExternalComponent("RNNCustomComponent", new FragmentCreator());
+
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            DefaultNewArchitectureEntryPoint.load();
+        }
     }
 
     @Override
