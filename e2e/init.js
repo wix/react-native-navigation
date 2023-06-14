ac6f774a23fd918e589f3e54ae39f89b7dcf833a
Yogev Ben David <yogev132@gmail.com>
2023-06-14 16:55:28 +0300

    Fix detox tests

--- e2e/init.js
+++ e2e/init.js
@@ -1,6 +1,13 @@
 const detox = require('detox');
 const config = require('../package.json').detox;
-require('detox-testing-library-rnn-adapter').extendDetox();
+// require('detox-testing-library-rnn-adapter').extendDetox();
+const isDetox = () => !!process.env.DETOX_START_TIMESTAMP;
+
+it.e2e = (name, fn) => {
+  if (isDetox()) {
+    it(name, fn);
+  }
+};
 
 jest.setTimeout(300000);
 
