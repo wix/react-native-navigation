157855ebb3af0b485b2783f03bd52c48845e238f
svbutko <svbutko@hotmail.com>
2023-06-15 12:11:00 +0300

    Fix metro config warning

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,14 @@
-module.exports = {
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
   projectRoot: `${__dirname}`,
   watchFolders: [__dirname],
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
