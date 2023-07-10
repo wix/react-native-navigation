746b42259f998e42fabece3b793b3d5bf7c20852
d4vidi <amit.d4vidi@gmail.com>
2023-07-10 12:18:04 +0300

    wip

--- lib/src/adapters/NativeEventsReceiver.ts
+++ lib/src/adapters/NativeEventsReceiver.ts
@@ -1,4 +1,5 @@
-import { NativeModules, NativeEventEmitter, EventEmitter, EmitterSubscription } from 'react-native';
+import { NativeModules, NativeEventEmitter, EmitterSubscription } from 'react-native';
+import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
 import {
   ComponentWillAppearEvent,
   ComponentDidAppearEvent,
