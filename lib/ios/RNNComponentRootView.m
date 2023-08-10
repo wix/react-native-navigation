858bf121be76fb87af40127b5443a4525779b427
yogevbd <yogev132@gmail.com>
2023-08-10 14:26:37 +0300

    Fix white screen flicker on push

--- lib/ios/RNNComponentRootView.m
+++ lib/ios/RNNComponentRootView.m
@@ -7,6 +7,21 @@
     self.sizeFlexibility = RCTRootViewSizeFlexibilityWidthAndHeight;
     return self;
 }
+
+- (instancetype)initWithBridge:(RCTBridge *)bridge
+                    moduleName:(NSString *)moduleName
+             initialProperties:(NSDictionary *)initialProperties
+                  eventEmitter:(RNNEventEmitter *)eventEmitter
+           reactViewReadyBlock:(RNNReactViewReadyCompletionBlock)reactViewReadyBlock {
+    self = [super initWithBridge:bridge
+                      moduleName:moduleName
+               initialProperties:initialProperties
+                    eventEmitter:eventEmitter
+             reactViewReadyBlock:reactViewReadyBlock];
+    [bridge.uiManager setAvailableSize:UIScreen.mainScreen.bounds.size forRootView:self];
+    return self;
+}
+
 - (NSString *)componentType {
     return ComponentTypeScreen;
 }
