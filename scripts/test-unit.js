301ae3621a710c9fe096b83b511575252bdb06e6
yogevbd <yogev132@gmail.com>
2023-08-15 15:09:41 +0300

    Fix ios unit tests

--- scripts/test-unit.js
+++ scripts/test-unit.js
@@ -25,7 +25,7 @@ function runAndroidUnitTests() {
 function runIosUnitTests() {
   exec.execSync('npm run build');
   exec.execSync('npm run pod-install');
-  testTarget('playground', 'iPhone 11', '13.7');
+  testTarget('playground', 'iPhone 14', '16.4');
   // testTarget('playgroundIOS12', 'iPhone X', '12.4');
 }
 
@@ -33,24 +33,14 @@ function testTarget(scheme, device, OS = 'latest') {
   const conf = release ? `Release` : `Debug`;
   exec.execSync(`cd ./playground/ios &&
   RCT_NO_LAUNCH_PACKAGER=true
-  xcodebuild build build-for-testing
-  -scheme "${scheme}"
-  -workspace playground.xcworkspace
-  -sdk iphonesimulator
-  -configuration ${conf}
-  -derivedDataPath ./DerivedData/playground
-  -UseModernBuildSystem=YES
-  ONLY_ACTIVE_ARCH=YES`);
-
-  exec.execSync(`cd ./playground/ios &&
-  RCT_NO_LAUNCH_PACKAGER=true
-  xcodebuild test-without-building
+  xcodebuild test
   -scheme "${scheme}"
   -workspace playground.xcworkspace
   -sdk iphonesimulator
   -configuration ${conf}
   -destination 'platform=iOS Simulator,name=${device},OS=${OS}'
   -derivedDataPath ./DerivedData/playground
+  -UseModernBuildSystem=YES
   ONLY_ACTIVE_ARCH=YES`);
 }
 
