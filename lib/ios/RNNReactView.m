558d6b3486e396476345fbe869eca9678a4524d5
Asaf Korem <asaf.korem@gmail.com>
2024-02-15 11:51:27 +0200

    fix(RN73 migration): replace obsolete react classes.

--- lib/ios/RNNReactView.m
+++ lib/ios/RNNReactView.m
@@ -1,6 +1,10 @@
 #import "RNNReactView.h"
 #import <React/RCTRootContentView.h>
 
+#ifdef RCT_NEW_ARCH_ENABLED
+#import <React/RCTFabricSurface.h>
+#endif
+
 @implementation RNNReactView {
     BOOL _isAppeared;
 }
@@ -11,10 +15,8 @@
                   eventEmitter:(RNNEventEmitter *)eventEmitter
                sizeMeasureMode:(RCTSurfaceSizeMeasureMode)sizeMeasureMode
            reactViewReadyBlock:(RNNReactViewReadyCompletionBlock)reactViewReadyBlock {
-    self = [super initWithBridge:bridge
-                      moduleName:moduleName
-               initialProperties:initialProperties
-                 sizeMeasureMode:sizeMeasureMode];
+  RCTFabricSurface *surface = [[RCTFabricSurface alloc] initWithBridge:bridge moduleName:moduleName initialProperties:initialProperties];
+  self = [super initWithSurface:surface sizeMeasureMode:sizeMeasureMode];
 #else
 - (instancetype)initWithBridge:(RCTBridge *)bridge
                     moduleName:(NSString *)moduleName
