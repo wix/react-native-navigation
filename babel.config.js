b7343afa9095182df2fff85a700d8e8117101b0d
Sayuri Mizuguchi <dev@sayurimizuguchi.com>
2024-08-22 12:02:55 -0400

    feat(upgrade): rn 74 requirement for babel plugin - exception treated

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,12 @@ module.exports = function (api) {
       '@babel/plugin-proposal-export-namespace-from',
       '@babel/plugin-proposal-export-default-from',
       'react-native-reanimated/plugin',
+      '@babel/plugin-proposal-class-properties',
+    ],
+    overrides: [
+      {
+        plugins: [['@babel/plugin-transform-private-methods']],
+      },
     ],
   };
 };
