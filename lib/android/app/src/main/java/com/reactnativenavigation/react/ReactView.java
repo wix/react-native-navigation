e719a90076678cb116bdcfed55eaab35a9b25d8a
Georgy Steshin <georgys@wix.com>
2025-01-29 17:15:04 +0200

    Downgrade to RN 76

--- lib/android/app/src/main/java/com/reactnativenavigation/react/ReactView.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/react/ReactView.java
@@ -36,7 +36,7 @@ public class ReactView extends ReactRootView implements IReactView, Renderable {
         this.componentId = componentId;
         this.componentName = componentName;
         jsTouchDispatcher = new JSTouchDispatcher(this);
-        setIsFabric(ReactNativeFeatureFlags.enableFabricRenderer());
+        setIsFabric(ReactFeatureFlags.enableFabricRenderer);
     }
 
     @Override
