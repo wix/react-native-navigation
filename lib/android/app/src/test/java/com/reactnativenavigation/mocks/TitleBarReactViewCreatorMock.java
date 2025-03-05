4162390a5e9842baa2e620d2cfde3a199343942d
d4vidi <amit.d4vidi@gmail.com>
2025-03-05 17:23:37 +0200

    Apply color animations on tab-switching (Android-only) (#7975)

--- lib/android/app/src/test/java/com/reactnativenavigation/mocks/TitleBarReactViewCreatorMock.java
+++ lib/android/app/src/test/java/com/reactnativenavigation/mocks/TitleBarReactViewCreatorMock.java
@@ -1,12 +1,12 @@
 package com.reactnativenavigation.mocks;
 
+import static org.mockito.Mockito.mock;
+
 import android.app.Activity;
 
 import com.facebook.react.ReactInstanceManager;
 import com.reactnativenavigation.views.stack.topbar.titlebar.TitleBarReactView;
 import com.reactnativenavigation.views.stack.topbar.titlebar.TitleBarReactViewCreator;
-
-import static org.mockito.Mockito.mock;
 
 public class TitleBarReactViewCreatorMock extends TitleBarReactViewCreator {
     public TitleBarReactViewCreatorMock() {
