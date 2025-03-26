4cfc9804f3dad0c8293f961e8d2b10a9799c1651
yogevbd <yogev132@gmail.com>
2025-03-27 00:57:01 +0200

    Fix buttons

--- lib/ios/RNNReactButtonView.mm
+++ lib/ios/RNNReactButtonView.mm
@@ -2,6 +2,25 @@
 
 @implementation RNNReactButtonView
 
+- (instancetype)initWithHost:(RCTHost *)host
+                  moduleName:(NSString *)moduleName
+           initialProperties:(NSDictionary *)initialProperties
+                eventEmitter:(RNNEventEmitter *)eventEmitter
+             sizeMeasureMode:(RCTSurfaceSizeMeasureMode)sizeMeasureMode
+         reactViewReadyBlock:(RNNReactViewReadyCompletionBlock)reactViewReadyBlock {
+    self = [super initWithHost:host moduleName:moduleName initialProperties:initialProperties eventEmitter:eventEmitter sizeMeasureMode:convertToSurfaceSizeMeasureMode(RCTRootViewSizeFlexibilityWidthAndHeight) reactViewReadyBlock:reactViewReadyBlock];
+    [host.surfacePresenter addObserver:self];
+    self.backgroundColor = UIColor.clearColor;
+
+    return self;
+}
+
+- (void)didMountComponentsWithRootTag:(NSInteger)rootTag {
+    if (self.surface.rootTag == rootTag) {
+        [self sizeToFit];
+    }
+}
+
 - (NSString *)componentType {
 	return ComponentTypeButton;
 }
--- lib/ios/RNNUIBarButtonItem.mm
+++ lib/ios/RNNUIBarButtonItem.mm
@@ -92,31 +92,6 @@
                            onPress:(RNNButtonPressCallback)onPress {
     self = [super initWithCustomView:reactView];
     [self applyOptions:buttonOptions];
-    reactView.sizeFlexibility = RCTRootViewSizeFlexibilityWidthAndHeight;
-    reactView.hidden = CGRectEqualToRect(reactView.frame, CGRectZero);
-
-    [NSLayoutConstraint deactivateConstraints:reactView.constraints];
-    self.widthConstraint =
-        [NSLayoutConstraint constraintWithItem:reactView
-                                     attribute:NSLayoutAttributeWidth
-                                     relatedBy:NSLayoutRelationEqual
-                                        toItem:nil
-                                     attribute:NSLayoutAttributeNotAnAttribute
-                                    multiplier:1.0
-                                      constant:reactView.intrinsicContentSize.width];
-    self.heightConstraint =
-        [NSLayoutConstraint constraintWithItem:reactView
-                                     attribute:NSLayoutAttributeHeight
-                                     relatedBy:NSLayoutRelationEqual
-                                        toItem:nil
-                                     attribute:NSLayoutAttributeNotAnAttribute
-                                    multiplier:1.0
-                                      constant:reactView.intrinsicContentSize.height];
-    [NSLayoutConstraint activateConstraints:@[ self.widthConstraint, self.heightConstraint ]];
-    reactView.delegate = self;
-
-    reactView.backgroundColor = [UIColor clearColor];
-
     self.onPress = onPress;
     return self;
 }
