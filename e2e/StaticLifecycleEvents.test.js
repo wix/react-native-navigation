52d5732109826e615618246574a3b1a8033db905
undefined <undefined>
2025-04-06 12:20:22 +0300

    comment ios test out

--- e2e/StaticLifecycleEvents.test.js
+++ e2e/StaticLifecycleEvents.test.js
@@ -54,7 +54,7 @@ describe('static lifecycle events', () => {
     await elementByLabel('OK').tap();
   });
 
-  xit('top bar buttons willAppear didAppear didDisappear', async () => {
+/*  xit('top bar buttons willAppear didAppear didDisappear', async () => {
     await elementById(TestIDs.PUSH_BTN).tap();
     await elementById(TestIDs.PUSH_OPTIONS_BUTTON).tap();
     await elementById(TestIDs.CLEAR_OVERLAY_EVENTS_BTN).tap();
@@ -70,7 +70,7 @@ describe('static lifecycle events', () => {
     await expect(
       elementByLabel('componentDidDisappear | CustomRoundedButton | TopBarButton')
     ).toBeVisible();
-  });
+  });*/
 
   it('top bar title willAppear didAppear didDisappear', async () => {
     await elementById(TestIDs.PUSH_BTN).tap();
