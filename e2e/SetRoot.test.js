d98e6cdcb11f09ef9aecbf960406bd4710353b08
Liat Netach <liatnetach@gmail.com>
2025-04-15 16:40:26 +0300

    update test

--- e2e/SetRoot.test.js
+++ e2e/SetRoot.test.js
@@ -52,15 +52,11 @@ describe('SetRoot', () => {
   });
 });
 
-  it(':ios: set root with left and right side menus - left menu visibility', async () => {
+  it(':ios: set root with left and right side menus - menu visibility', async () => {
     await elementById(TestIDs.SET_ROOT_WITH_MENUS).tap();
     await elementById(TestIDs.OPEN_LEFT_SIDE_MENU_BTN).tap();
     await elementById(TestIDs.CLOSE_LEFT_SIDE_MENU_BTN).tap();
     await expect(elementById(TestIDs.CLOSE_LEFT_SIDE_MENU_BTN)).toBeNotVisible();
-  });
-
-  it(':ios: set root with left and right side menus - right menu visibility', async () => {
-    await elementById(TestIDs.SET_ROOT_WITH_MENUS).tap();
     await elementById(TestIDs.OPEN_RIGHT_SIDE_MENU_BTN).tap();
     await elementById(TestIDs.CLOSE_RIGHT_SIDE_MENU_BTN).tap();
     await expect(elementById(TestIDs.CLOSE_RIGHT_SIDE_MENU_BTN)).toBeNotVisible();
