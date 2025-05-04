1ee37472340e90c564fd03a25deed4c4529c6544
d4vidi <amit.d4vidi@gmail.com>
2025-05-04 18:13:27 +0300

    Mocked side-menus - support (#8017)

--- lib/Mock/Layouts/LayoutNodeFactory.ts
+++ lib/Mock/Layouts/LayoutNodeFactory.ts
@@ -2,6 +2,11 @@ import BottomTabs from './BottomTabsNode';
 import ComponentNode from './ComponentNode';
 import Stack from './StackNode';
 import ParentNode from './ParentNode';
+import SideMenuRootNode, {
+  SideMenuLeftNode,
+  SideMenuRightNode,
+  SideMenuCenterNode,
+} from './SideMenu';
 
 export default class LayoutNodeFactory {
   static create(layout: any, parentNode?: ParentNode) {
@@ -10,7 +15,15 @@ export default class LayoutNodeFactory {
         return new ComponentNode(layout, parentNode);
       case 'Stack':
         return new Stack(layout, parentNode);
-      default:
+      case 'SideMenuRoot':
+        return new SideMenuRootNode(layout, parentNode);
+      case 'SideMenuLeft':
+        return new SideMenuLeftNode(layout, parentNode);
+      case 'SideMenuCenter':
+        return new SideMenuCenterNode(layout, parentNode);
+      case 'SideMenuRight':
+        return new SideMenuRightNode(layout, parentNode);
+      default: // TODO Undo
       case 'BottomTabs':
         return new BottomTabs(layout, parentNode);
     }
