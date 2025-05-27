5750bd3b3d3ba77951ce9d9dc29a509dec8d33e7
Georgy Steshin <georgys@wix.com>
2025-05-27 14:08:05 +0300

    Fixed more unit tests

--- lib/android/app/src/test/java/com/reactnativenavigation/options/LayoutNodeParserTest.java
+++ lib/android/app/src/test/java/com/reactnativenavigation/options/LayoutNodeParserTest.java
@@ -8,8 +8,7 @@ import org.junit.*;
 
 import static org.assertj.core.api.Java6Assertions.*;
 
-@Ignore("New architecture - WIP")
-public class LayoutNodeParserTest extends BaseTest {
+public class LayoutNodeParserTest extends BaseRobolectricTest {
     @Test
     public void dto() throws Exception {
         LayoutNode node = new LayoutNode("the id", LayoutNode.Type.Component);
