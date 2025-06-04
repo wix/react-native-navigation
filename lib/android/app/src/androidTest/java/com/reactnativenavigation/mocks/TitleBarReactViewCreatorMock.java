2872fc0f093edd9467e5efd84edfe10f7a08a491
Georgy Steshin <georgys@wix.com>
2025-06-04 14:04:46 +0300

    More Android Unitests

---
+++ lib/android/app/src/androidTest/java/com/reactnativenavigation/mocks/TitleBarReactViewCreatorMock.java
@@ -0,0 +1,8 @@
+package com.reactnativenavigation.mocks;
+
+import com.reactnativenavigation.views.stack.topbar.titlebar.TitleBarReactViewCreator;
+
+public class TitleBarReactViewCreatorMock extends TitleBarReactViewCreator {
+    public TitleBarReactViewCreatorMock() {
+    }
+}
