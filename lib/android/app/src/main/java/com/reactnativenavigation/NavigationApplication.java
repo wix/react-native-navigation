12d212cd13ba2f752d4d5e7a06f8dc05f24bb822
Georgy Steshin <gosha212@gmail.com>
2025-06-11 19:51:30 +0300

    Improve Documentation for RNN Installation (#8042)
    * Converted a MainActivity and MainApplication to kotlin.
    Refined installation documentation
    
    * Added swift documentation
    
    * Fixes after PR

--- lib/android/app/src/main/java/com/reactnativenavigation/NavigationApplication.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/NavigationApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.soloader.OpenSourceMergedSoMapping;
 import com.facebook.soloader.SoLoader;
 import com.reactnativenavigation.react.ReactGateway;
@@ -40,6 +41,9 @@ public abstract class NavigationApplication extends Application implements React
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        DefaultNewArchitectureEntryPoint.load();
+
         reactGateway = createReactGateway();
 	}
 
