5cad1294fefd0f2ff8399611235b4642cef58642
d4vidi <amit.d4vidi@gmail.com>
2025-07-17 17:38:02 +0300

    Revert unnecessary changes

--- lib/android/app/src/main/java/com/reactnativenavigation/views/component/ComponentLayout.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/views/component/ComponentLayout.java
@@ -1,22 +1,23 @@
 package com.reactnativenavigation.views.component;
 
-import static com.reactnativenavigation.utils.CoordinatorLayoutUtils.matchParentLP;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.MotionEvent;
 import android.view.ViewGroup;
-
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import android.view.WindowInsets;
 
 import com.reactnativenavigation.options.ButtonOptions;
+import com.reactnativenavigation.viewcontrollers.viewcontroller.ScrollEventListener;
 import com.reactnativenavigation.options.Options;
 import com.reactnativenavigation.options.params.Bool;
 import com.reactnativenavigation.react.ReactView;
 import com.reactnativenavigation.react.events.ComponentType;
 import com.reactnativenavigation.viewcontrollers.stack.topbar.button.ButtonController;
-import com.reactnativenavigation.viewcontrollers.viewcontroller.ScrollEventListener;
 import com.reactnativenavigation.views.touch.OverlayTouchDelegate;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import static com.reactnativenavigation.utils.CoordinatorLayoutUtils.matchParentLP;
 
 @SuppressLint("ViewConstructor")
 public class ComponentLayout extends CoordinatorLayout implements ReactComponent, ButtonController.OnClickListener {
@@ -98,6 +99,7 @@ public class ComponentLayout extends CoordinatorLayout implements ReactComponent
     public void onPress(ButtonOptions button) {
         reactView.sendOnNavigationButtonPressed(button.id);
     }
+
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
