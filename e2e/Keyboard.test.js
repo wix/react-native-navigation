5147abfb75b9a56e4721795df6b0e33bed42c4f9
d4vidi <amit.d4vidi@gmail.com>
2025-08-14 14:20:43 +0300

    e2e(kbd): fill-in ancient test gaps

--- e2e/Keyboard.test.js
+++ e2e/Keyboard.test.js
@@ -1,11 +1,59 @@
 import { default as TestIDs, default as testIDs } from '../playground/src/testIDs';
+import { device } from 'detox';
 import Utils from './Utils';
 
 const { elementByLabel, elementById } = Utils;
 
 const KBD_OBSCURED_TEXT = 'Keyboard Demo';
 
+const enableOnScreenKeyboard = async (adb, adbName) =>
+  adb.shell(adbName, 'settings put Secure show_ime_with_hard_keyboard 1');
+
+const disableOnScreenKeyboard = async (adb, adbName) =>
+  adb.shell(adbName, 'settings put Secure show_ime_with_hard_keyboard 0');
+
+const driver = {
+  async init() {
+    const { id: adbName } = device;
+    const { adb } = device.deviceDriver;
+
+    if (!adb || !adbName) {
+      throw new Error(`Keyboard driver init failed (id=${adbName}, hasADB=${!!adb})`);
+    }
+
+    this.adb = adb;
+    this.adbName = adbName;
+    this.kbdEnabled = await adb.shell(adbName, 'settings get Secure show_ime_with_hard_keyboard');
+
+    if (!(this.kbdEnabled === '0') || !(this.kbdEnabled === '1')) {
+      console.warn('[KeyboardDriver] Unable to get on-screen KBD setting, defaulting to false');
+      this.kbdEnabled = '0'
+    }
+  },
+
+  async enableOnScreenKeyboard() {
+    await this.adb.shell(this.adbName, 'settings put Secure show_ime_with_hard_keyboard 1');
+  },
+  async restoreOnScreenKeyboard() {
+    if (!this.adb) {
+      // Not initialized
+      return;
+    }
+
+    await this.adb.shell(this.adbName, `settings put Secure show_ime_with_hard_keyboard ${this.kbdEnabled}`);
+  },
+}
+
 describe.e2e('Keyboard', () => {
+  beforeAll(async () => {
+    await driver.init();
+    await driver.enableOnScreenKeyboard();
+  });
+
+  afterAll(async () => {
+    await driver.restoreOnScreenKeyboard();
+  });
+
   beforeEach(async () => {
     await device.launchApp({ newInstance: true });
     await elementById(TestIDs.KEYBOARD_SCREEN_BTN).tap();
@@ -49,7 +97,7 @@ describe.e2e('Keyboard', () => {
     await expect(elementById(TestIDs.TEXT_INPUT1)).not.toBeFocused();
   });
 
-  it(':android should respect UI with keyboard awareness', async () => {
+  it(':android: should respect UI with keyboard awareness', async () => {
     await elementById(TestIDs.PUSH_KEYBOARD_SCREEN_STICKY_FOOTER).tap();
     await elementById(TestIDs.TEXT_INPUT2).tap();
     await expect(elementByLabel(KBD_OBSCURED_TEXT)).toBeVisible();
