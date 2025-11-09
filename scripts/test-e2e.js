9e2c22c9db7d32cb886447a8fda5d1bb11860321
d4vidi <amit.d4vidi@gmail.com>
2025-11-09 17:32:01 +0200

    ci(android): Switch to google emu, android15

--- scripts/test-e2e.js
+++ scripts/test-e2e.js
@@ -12,10 +12,10 @@ const CI = includes(process.argv, '--ci');
 run();
 
 function run() {
-  const prefix = android ? (CI ? `android.genycloud` : `android.emu`) : `ios.sim`;
-  const suffix = release ? `release` : `debug`;
+  const prefix = android ? 'android.emu' : 'ios.sim';
+  const suffix = release ? 'release' : 'debug';
   const configuration = `${prefix}.${suffix}`;
-  const headless$ = android ? (headless ? `--headless` : ``) : ``;
+  const headless$ = android ? (headless ? '--headless' : '') : '';
   const workers = multi ? 3 : 1;
   const loglevel = verbose ? '--loglevel verbose' : '';
 
