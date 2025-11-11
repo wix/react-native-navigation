656697092d47422897d7e0cfcb9cdc2666196ac9
Georgy Steshin <gosha212@gmail.com>
2025-11-11 16:53:11 +0200

    Before test fix

--- jest.config.js
+++ jest.config.js
@@ -12,14 +12,14 @@ module.exports = {
     '<rootDir>/playground/src/',
     '<rootDir>/integration/',
     '<rootDir>/scripts/',
-    '<rootDir>/e2e/',
+    '<rootDir>/playground/e2e/',
     '<rootDir>/autolink/',
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect', './jest-setup.js'],
   testPathIgnorePatterns: ['/node_modules/'],
   moduleNameMapper: {
-    'react-native-navigation/Mock': '<rootDir>/lib/Mock',
-    'react-native-navigation': '<rootDir>/lib/src',
+    'react-native-navigation/Mock': '<rootDir>/Mock',
+    'react-native-navigation': '<rootDir>/src',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/playground/img/layouts@2x.png',
   },
