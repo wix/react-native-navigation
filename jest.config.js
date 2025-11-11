9b5ed78df25ef42db359e4d622b910974182e021
Georgy Steshin <gosha212@gmail.com>
2025-11-11 16:56:55 +0200

    Fixed mock

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect', './jest-setup.js'],
   testPathIgnorePatterns: ['/node_modules/'],
   moduleNameMapper: {
-    'react-native-navigation/Mock': '<rootDir>/Mock',
+    'react-native-navigation/Mock': '<rootDir>/Mock/index',
     'react-native-navigation': '<rootDir>/src',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/playground/img/layouts@2x.png',
