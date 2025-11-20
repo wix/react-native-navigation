f2d3c0c75cf34a3ad76d65ab26f2ede55d10abf0
Mark de Vocht <markd@wix.com>
2025-11-20 13:58:25 +0200

    AppDelegate updates

--- ios/RNNAppDelegate.h
+++ ios/RNNAppDelegate.h
@@ -1,13 +1,45 @@
 #import <Foundation/Foundation.h>
 #import <React/CoreModulesPlugins.h>
+#import <ReactNativeNavigation/ReactNativeVersionExtracted.h>
 
+#if RN_VERSION_MAJOR == 0 && RN_VERSION_MINOR < 79
 #if __has_include(<React-RCTAppDelegate/RCTAppDelegate.h>)
 #import <React-RCTAppDelegate/RCTAppDelegate.h>
 #elif __has_include(<React_RCTAppDelegate/RCTAppDelegate.h>)
-// for importing the header from framework, the dash will be transformed to underscore
 #import <React_RCTAppDelegate/RCTAppDelegate.h>
 #endif
 
+#import <React/RCTBridge.h>
+#else
+#if __has_include(<React-RCTAppDelegate/RCTDefaultReactNativeFactoryDelegate.h>)
+#import <React-RCTAppDelegate/RCTDefaultReactNativeFactoryDelegate.h>
+#elif __has_include(<React_RCTAppDelegate/RCTDefaultReactNativeFactoryDelegate.h>)
+#import <React_RCTAppDelegate/RCTDefaultReactNativeFactoryDelegate.h>
+#endif
+#endif
+
+#if __has_include(<ReactAppDependencyProvider/RCTAppDependencyProvider.h>)
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
+#endif
+
+#if __has_include(<React-RCTAppDelegate/RCTReactNativeFactory.h>)
+#import <React-RCTAppDelegate/RCTReactNativeFactory.h>
+#elif __has_include(<React_RCTAppDelegate/RCTReactNativeFactory.h>)
+#import <React_RCTAppDelegate/RCTReactNativeFactory.h>
+#endif
+
+#import <React/RCTBundleURLProvider.h>
+
+#if RN_VERSION_MAJOR == 0 && RN_VERSION_MINOR < 79
 @interface RNNAppDelegate : RCTAppDelegate
+#else
+@interface RNNAppDelegate : UIResponder <UIApplicationDelegate>
+@property(nonatomic, strong) UIWindow *window;
+
+@property(nonatomic, strong) RCTDefaultReactNativeFactoryDelegate *reactNativeDelegate;
+
+@property(nonatomic, strong) RCTReactNativeFactory *reactNativeFactory;
+@property(nonatomic) BOOL bridgelessEnabled;
+#endif
 
 @end
--- ios/RNNAppDelegate.mm
+++ ios/RNNAppDelegate.mm
@@ -1,9 +1,7 @@
-
 #import "RNNAppDelegate.h"
 #import <ReactNativeNavigation/ReactNativeNavigation.h>
 #import <react/featureflags/ReactNativeFeatureFlags.h>
 #import <react/featureflags/ReactNativeFeatureFlagsDefaults.h>
-
 
 #import "RCTAppSetupUtils.h"
 #import <React/CoreModulesPlugins.h>
@@ -14,17 +12,7 @@
 #import <React/RCTSurfacePresenterBridgeAdapter.h>
 #import <ReactCommon/RCTTurboModuleManager.h>
 
-
-
-#if __has_include(<React-RCTAppDelegate/RCTReactNativeFactory.h>)
-#import <React-RCTAppDelegate/RCTAppDelegate.h>
-#import <React-RCTAppDelegate/RCTReactNativeFactory.h>
-#elif __has_include(<React_RCTAppDelegate/RCTReactNativeFactory.h>)
-#import <React_RCTAppDelegate/RCTAppDelegate.h>
-#import <React_RCTAppDelegate/RCTReactNativeFactory.h>
-#else
-// RN 0.77 support
-#define RN077
+#if __has_include(<react/config/ReactNativeConfig.h>)
 #import <react/config/ReactNativeConfig.h>
 #endif
 
@@ -39,100 +27,113 @@
 
 #import <React/RCTComponentViewFactory.h>
 
+
 static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
+#if RN_VERSION_MAJOR == 0 && RN_VERSION_MINOR < 79
 @interface RNNAppDelegate () <RCTTurboModuleManagerDelegate,
-                              RCTComponentViewFactoryComponentProvider> {
+RCTComponentViewFactoryComponentProvider> {
 }
 @end
+#else
+@interface RNNAppDelegate () {
+}
+@end
+#endif
 
 @implementation RNNAppDelegate
 
 - (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-#ifdef RN077
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+#if RN_VERSION_MAJOR == 0 && RN_VERSION_MINOR < 79
     [self _setUpFeatureFlags];
-    self.rootViewFactory = [self createRCTRootViewFactory];
+    self.reactNativeFactory.rootViewFactory = [self createRCTRootViewFactory];
     [RCTComponentViewFactory currentComponentViewFactory].thirdPartyFabricComponentsProvider = self;
     RCTAppSetupPrepareApp(application, self.newArchEnabled);
     RCTSetNewArchEnabled(TRUE);
-#else
-    self.reactNativeFactory = [RCTReactNativeFactory new];
-    self.reactNativeFactory = [self.reactNativeFactory initWithDelegate:self];
-#endif
     
     RCTEnableTurboModuleInterop(YES);
     RCTEnableTurboModuleInteropBridgeProxy(YES);
-
+    
     self.rootViewFactory.reactHost = [self.rootViewFactory createReactHost:launchOptions];
-
+    
     [ReactNativeNavigation bootstrapWithHost:self.rootViewFactory.reactHost];
-
+#else
+    self.reactNativeFactory = [[RCTReactNativeFactory alloc] initWithDelegate:self.reactNativeDelegate];
+    self.reactNativeDelegate.dependencyProvider = [RCTAppDependencyProvider new];
+    
+    RCTAppSetupPrepareApp(application, YES);
+    RCTEnableTurboModuleInteropBridgeProxy(YES);
+    
+    self.reactNativeFactory.rootViewFactory.reactHost = [self.reactNativeFactory.rootViewFactory createReactHost:launchOptions];
+    
+    [ReactNativeNavigation bootstrapWithHost:self.reactNativeFactory.rootViewFactory.reactHost];
+#endif
+    
     return YES;
 }
 
+
+#if RN_VERSION_MAJOR == 0 && RN_VERSION_MINOR < 79
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge {
-	[NSException raise:@"RCTBridgeDelegate::sourceURLForBridge not implemented"
-				format:@"Subclasses must implement a valid sourceURLForBridge method"];
-	return nil;
+    [NSException raise:@"RCTBridgeDelegate::sourceURLForBridge not implemented"
+                format:@"Subclasses must implement a valid sourceURLForBridge method"];
+    return nil;
 }
 
 - (BOOL)concurrentRootEnabled {
-	return true;
+    return true;
 }
 
-
-
-#ifdef RN077
 - (RCTRootViewFactory *)createRCTRootViewFactory
 {
-  __weak __typeof(self) weakSelf = self;
-  RCTBundleURLBlock bundleUrlBlock = ^{
-    RCTAppDelegate *strongSelf = weakSelf;
-    return strongSelf.bundleURL;
-  };
-
-  RCTRootViewFactoryConfiguration *configuration =
-      [[RCTRootViewFactoryConfiguration alloc] initWithBundleURLBlock:bundleUrlBlock
-                                                       newArchEnabled:self.newArchEnabled];
-
-
-  return [[RCTRootViewFactory alloc] initWithConfiguration:configuration andTurboModuleManagerDelegate:self];
+    __weak __typeof(self) weakSelf = self;
+    RCTBundleURLBlock bundleUrlBlock = ^{
+        RCTAppDelegate *strongSelf = weakSelf;
+        return strongSelf.bundleURL;
+    };
+    
+    RCTRootViewFactoryConfiguration *configuration =
+    [[RCTRootViewFactoryConfiguration alloc] initWithBundleURLBlock:bundleUrlBlock
+                                                     newArchEnabled:self.newArchEnabled];
+    
+    
+    return [[RCTRootViewFactory alloc] initWithConfiguration:configuration andTurboModuleManagerDelegate:self];
 }
-
 
 #pragma mark - Feature Flags
 class RCTAppDelegateBridgelessFeatureFlags : public facebook::react::ReactNativeFeatureFlagsDefaults {
- public:
-  bool enableBridgelessArchitecture() override
-  {
-    return true;
-  }
-  bool enableFabricRenderer() override
-  {
-    return true;
-  }
-  bool useTurboModules() override
-  {
-    return true;
-  }
-  bool useNativeViewConfigsInBridgelessMode() override
-  {
-    return true;
-  }
-  bool enableFixForViewCommandRace() override
-  {
-    return true;
-  }
+public:
+    bool enableBridgelessArchitecture() override
+    {
+        return true;
+    }
+    bool enableFabricRenderer() override
+    {
+        return true;
+    }
+    bool useTurboModules() override
+    {
+        return true;
+    }
+    bool useNativeViewConfigsInBridgelessMode() override
+    {
+        return true;
+    }
+    
+    
+    bool enableFixForViewCommandRace() override
+    {
+        return true;
+    }
 };
 
 - (void)_setUpFeatureFlags
 {
     facebook::react::ReactNativeFeatureFlags::override(
-        std::make_unique<RCTAppDelegateBridgelessFeatureFlags>());
+                                                       std::make_unique<RCTAppDelegateBridgelessFeatureFlags>());
 }
 #endif
 
 @end
-
