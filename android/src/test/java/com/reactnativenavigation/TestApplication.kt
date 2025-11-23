f1088c8e791aa612e691f69af11ab615ad31329a
Georgy Steshin <gosha212@gmail.com>
2025-11-23 14:49:13 +0200

    Fixed roboletric tests

--- android/src/test/java/com/reactnativenavigation/TestApplication.kt
+++ android/src/test/java/com/reactnativenavigation/TestApplication.kt
@@ -1,43 +1,29 @@
-package com.reactnativenavigation;
+package com.reactnativenavigation
 
-import android.app.Application;
+import android.app.Application
+import androidx.appcompat.R
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactHost;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.defaults.DefaultReactHost;
-
-import java.util.Collections;
-import java.util.List;
-
-
-public class TestApplication extends Application implements ReactApplication {
-    private final ReactNativeHost host = new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return true;
+class TestApplication : Application(), ReactApplication {
+    override val reactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean {
+            return true
         }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-            return Collections.emptyList();
+        override fun getPackages(): MutableList<ReactPackage> {
+            return mutableListOf()
         }
-    };
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        setTheme(androidx.appcompat.R.style.Theme_AppCompat);
     }
 
-    @Override
-    public ReactNativeHost getReactNativeHost() {
-        return host;
+    override fun onCreate() {
+        super.onCreate()
+        setTheme(R.style.Theme_AppCompat)
     }
 
-    @Override
-    public ReactHost getReactHost() {
-        return DefaultReactHost.getDefaultReactHost(this, getReactNativeHost());
-    }
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(this, reactNativeHost)
 }
