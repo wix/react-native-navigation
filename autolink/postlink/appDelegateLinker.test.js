04455889bcff4f708f9f5c1c1fd404582ee4b49b
Mark de Vocht <markd@wix.com>
2025-11-24 18:58:18 +0200

    link test update for iOS

--- autolink/postlink/appDelegateLinker.test.js
+++ autolink/postlink/appDelegateLinker.test.js
@@ -42,4 +42,24 @@ describe('appDelegateLinker', () => {
     const appDelegateContent = fs.readFileSync(linker.appDelegatePath, 'utf8');
     expect(appDelegateContent).toMatchSnapshot();
   });
+
+  it('should work with Swift bridgeless RN 0.79+', () => {
+    jest.mock('./path', () => {
+      const tmpAppDelegatePath = mockHelpers.prepareFixtureDuplicate79({
+        userFixtureFileName: 'AppDelegate.swift.template',
+        patchedFixtureFileName: 'rnn-tests_AppDelegate79.swift',
+      });
+
+      return {
+        appDelegate: tmpAppDelegatePath,
+      };
+    });
+
+    const AppDelegateLinker = require('./appDelegateLinker');
+    const linker = new AppDelegateLinker();
+    linker.link();
+
+    const appDelegateContent = fs.readFileSync(linker.appDelegatePath, 'utf8');
+    expect(appDelegateContent).toMatchSnapshot();
+  });
 });
