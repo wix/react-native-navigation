746ab40ac07aed3d1edaad2593fdab0036f90ce0
Georgy Steshin <gosha212@gmail.com>
2025-12-28 10:55:26 +0200

    Update Buildkite pipeline scripts for React Native 0.82 support

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -5,12 +5,10 @@ echo "steps:"
 cat .buildkite/jobs/pipeline.release.yml
 cat .buildkite/jobs/pipeline.android_rn_77.yml
 cat .buildkite/jobs/pipeline.android_rn_78.yml
-cat .buildkite/jobs/pipeline.android_rn_80.yml
-cat .buildkite/jobs/pipeline.android_rn_81.yml
+cat .buildkite/jobs/pipeline.android_rn_82.yml
 cat .buildkite/jobs/pipeline.ios_rn_77.yml
 cat .buildkite/jobs/pipeline.ios_rn_78.yml
-cat .buildkite/jobs/pipeline.ios_rn_80.yml
-cat .buildkite/jobs/pipeline.ios_rn_81.yml
+cat .buildkite/jobs/pipeline.ios_rn_82.yml
 cat .buildkite/jobs/pipeline.publish.yml
 
 
