cecaa13f4befb0e27a0f425dad815ec5c9c01480
Georgy Steshin <gosha212@gmail.com>
2025-12-28 09:45:12 +0200

    Support RN 0.81 (#8186)
    * Support RN 0.81
    
    * Fixed roboletric tests on RN 0.81.5

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -5,12 +5,12 @@ echo "steps:"
 cat .buildkite/jobs/pipeline.release.yml
 cat .buildkite/jobs/pipeline.android_rn_77.yml
 cat .buildkite/jobs/pipeline.android_rn_78.yml
-cat .buildkite/jobs/pipeline.android_rn_79.yml
 cat .buildkite/jobs/pipeline.android_rn_80.yml
+cat .buildkite/jobs/pipeline.android_rn_81.yml
 cat .buildkite/jobs/pipeline.ios_rn_77.yml
 cat .buildkite/jobs/pipeline.ios_rn_78.yml
-cat .buildkite/jobs/pipeline.ios_rn_79.yml
 cat .buildkite/jobs/pipeline.ios_rn_80.yml
+cat .buildkite/jobs/pipeline.ios_rn_81.yml
 cat .buildkite/jobs/pipeline.publish.yml
 
 
