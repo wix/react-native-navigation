11f406a59f3d168cce316c22e358688ccd4e08f0
Georgy Steshin <gosha212@gmail.com>
2026-01-05 15:46:31 +0200

    Feat/8190 support react native 083 (#8191)
    * Support for RN 0.83
    
    * Updated react version
    
    * Fix missing newline at end of package.json files in root and playground directories
    
    * Update gradle wrapper
    
    * Upgrade to rn 0.83
    
    * Added 0.83 pipelines
    
    * Removed usage of CSSBackgroundDrawable
    
    * Fixed docs
    
    * Update website/docs/docs/docs-Installing.mdx
    
    Co-authored-by: d4vidi <amit.d4vidi@gmail.com>
    
    * Update website/docs/docs/docs-Installing.mdx
    
    Co-authored-by: d4vidi <amit.d4vidi@gmail.com>
    
    ---------
    
    Co-authored-by: d4vidi <amit.d4vidi@gmail.com>

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -6,7 +6,9 @@ cat .buildkite/jobs/pipeline.release.yml
 cat .buildkite/jobs/pipeline.android_rn_77.yml
 cat .buildkite/jobs/pipeline.android_rn_78.yml
 cat .buildkite/jobs/pipeline.android_rn_82.yml
+cat .buildkite/jobs/pipeline.android_rn_83.yml
 cat .buildkite/jobs/pipeline.ios_rn_77.yml
 cat .buildkite/jobs/pipeline.ios_rn_78.yml
 cat .buildkite/jobs/pipeline.ios_rn_82.yml
+cat .buildkite/jobs/pipeline.ios_rn_83.yml
 cat .buildkite/jobs/pipeline.publish.yml
