c8260c682fc3c28f8b5728f42b72a0b39009c840
Mark de Vocht <markd@wix.com>
2026-02-25 20:10:48 +0200

    possible fix

--- android/src/main/java/com/reactnativenavigation/views/stack/topbar/titlebar/TitleBarReactView.kt
+++ android/src/main/java/com/reactnativenavigation/views/stack/topbar/titlebar/TitleBarReactView.kt
@@ -3,25 +3,22 @@ package com.reactnativenavigation.views.stack.topbar.titlebar
 import android.annotation.SuppressLint
 import android.content.Context
 import android.view.View
-import android.view.ViewGroup
-import androidx.core.view.children
-import com.facebook.react.ReactInstanceManager
 import com.reactnativenavigation.react.ReactView
 
 @SuppressLint("ViewConstructor")
 class TitleBarReactView(context: Context?, componentId: String?,
                         componentName: String?) : ReactView(context, componentId, componentName) {
     var centered: Boolean = false
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        var titleHeightMeasureSpec: Int
-        var titleWidthMeasureSpec: Int
         if (centered) {
-            titleHeightMeasureSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
-            titleWidthMeasureSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
+            val availableWidth = MeasureSpec.getSize(widthMeasureSpec)
+            super.onMeasure(
+                MeasureSpec.makeMeasureSpec(availableWidth, MeasureSpec.EXACTLY),
+                heightMeasureSpec
+            )
         } else {
-            titleHeightMeasureSpec = heightMeasureSpec
-            titleWidthMeasureSpec = widthMeasureSpec
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         }
-        super.onMeasure(titleWidthMeasureSpec, titleHeightMeasureSpec)
     }
 }
