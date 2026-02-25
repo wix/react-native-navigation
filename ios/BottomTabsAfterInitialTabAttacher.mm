52d67f8659884d5db55dcaee69360df18dd327bb
Mark de Vocht <markd@wix.com>
2026-02-25 12:41:32 +0200

    Have also the afterInitialTab flag on page tabs handle webviews.

--- ios/BottomTabsAfterInitialTabAttacher.mm
+++ ios/BottomTabsAfterInitialTabAttacher.mm
@@ -8,7 +8,38 @@
       [bottomTabsController readyForPresentation];
       for (UIViewController *viewController in bottomTabsController.deselectedViewControllers) {
         dispatch_async(dispatch_get_main_queue(), ^{
+          UIWindow *preloadWindow = [[UIWindow alloc] initWithFrame:CGRectZero];
+          preloadWindow.hidden = NO;
+
+          dispatch_group_t ready = dispatch_group_create();
+          dispatch_group_enter(ready);
+
+          viewController.waitForRender = YES;
+
+          [viewController setReactViewReadyCallback:^{
+            dispatch_group_leave(ready);
+          }];
+
           [viewController render];
+
+          UIView *containerView = nil;
+          UIView *reactView = nil;
+          if ([viewController isKindOfClass:[UINavigationController class]]) {
+              containerView = [(UINavigationController *)viewController topViewController].view;
+              reactView = containerView.subviews.firstObject;
+          }
+
+          if (reactView && !reactView.window) {
+              [preloadWindow addSubview:reactView];
+          }
+
+          dispatch_notify(ready, dispatch_get_main_queue(), ^{
+            if (reactView) {
+                reactView.frame = containerView.bounds;
+                [containerView addSubview:reactView];
+            }
+            preloadWindow.hidden = YES;
+          });
         });
       }
     }];
